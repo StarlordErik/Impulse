f7e2dbd3a95c71d5ee85df27ae3fdfc21fd34d26
StarlordErik <erik.daas.apps@outlook.de>
2025-05-21 16:46:36 +0200

    Abhängigkeiten aktualisiert, build erfolgreich durchgeführt und Hello World auf 2 Geräten gesehen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.impulse"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.impulse"
+        applicationId = "de.seleri.impulse"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
