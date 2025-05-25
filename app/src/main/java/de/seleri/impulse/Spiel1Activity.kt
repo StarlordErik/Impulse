a61ae74a32abaf993bbfca85111c851b207e4f68
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-05-25 17:02:41 +0200

    einfacher Startbildschirm steht

---
+++ app/src/main/java/de/seleri/impulse/Spiel1Activity.kt
@@ -0,0 +1,12 @@
+package de.seleri.impulse
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.impulse.R
+
+class Spiel1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spiel1)
+    }
+}
---
+++ app/src/main/java/de/seleri/impulse/Spiel2Activity.kt
@@ -0,0 +1,12 @@
+package de.seleri.impulse
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.impulse.R
+
+class Spiel2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spiel2)
+    }
+}
