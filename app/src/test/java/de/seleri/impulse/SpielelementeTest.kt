465276ee28c161e01bd0633a163d054826ddf979
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-06-07 22:52:11 +0200

    Wir testen, ob der Unit Test Workflow funktioniert

---
+++ app/src/test/java/de/seleri/impulse/SpielelementeTest.kt
@@ -0,0 +1,12 @@
+package de.seleri.impulse
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class SpielelementeTest {
+
+    @Test
+    fun funktioniertKotlin() {
+        assertEquals(3, 2 + 2)
+    }
+}
