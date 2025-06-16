67c64c432441248f296a1b0b55c56233ec21c9aa
StarlordErik <erik.daas.apps@outlook.de>
2025-06-16 19:17:55 +0200

    Documentation für LokalisierbaresSpielelement.kt nachgeholt

--- app/src/main/java/de/seleri/spielelemente/LokalisierbaresSpielelement.kt
+++ app/src/main/java/de/seleri/spielelemente/LokalisierbaresSpielelement.kt
@@ -2,38 +2,93 @@ package de.seleri.spielelemente
 
 import kotlin.collections.set
 
+/**
+ * Abstrakte Basisklasse für alle Spielelemente mit ID und den localized Kartentexten/Namen
+ */
 abstract class LokalisierbaresSpielelement(
-    open val id: Int, open val localizations: MutableMap<Sprachen, String>
+    open val id: Int,
+
+    // enthält zu allen Sprachen-Enum-Einträgen den jeweiligen Kartentext/Namen
+    open val localizations: MutableMap<Sprachen, String>
 ) {
+    /**
+     * Konvertiert die ID ins YAML-Format und beginnt damit den YAML-Abschnitt eines Elements.
+     *
+     * @return nur die ID als YAML-String
+     */
     open fun toYaml(): String {
         val output = StringBuilder()
         output.append(attributToYamlZeile(1, "- $ID", id))
         return output.toString()
     }
 
+    /**
+     * Konvertiert die localizations ins YAML-Format.
+     *
+     * @param bezeichnung Kartentext oder Name einer Sammlung (Kategorie, Spiel)
+     * @return "Bezeichnung: localizedTexts" als YAML-String
+     */
     open fun localizationsToYaml(bezeichnung: String): String {
         return attributToYamlZeile(2, bezeichnung, localizations)
     }
 
+    /**
+     * Setzt die Übersetzung für eine Sprache, sofern diese nicht OG (Original) ist.
+     *
+     * @param sprache Sprache, in der die Übersetzung gespeichert werden soll
+     * @param bezeichnung Übersetzung des Textes / der Name
+     */
     open fun setUebersetzung(sprache: Sprachen, bezeichnung: String) {
         if (sprache != Sprachen.OG) localizations[sprache] = bezeichnung
     }
 
     companion object {
+
+        /**
+         * Erstellt ein Spielelement aus Eingabedaten von der Konsole bzw. eines Skripts.
+         *
+         * @param id neue, noch nicht vergebene ID
+         * @param sprache Sprache des Textes
+         * @param bezeichnung Kartentext / Name
+         *
+         * @return Tupel (id, localizations) aka abstraktes Objekt von LokalisierbaresElement
+         */
         fun fromEingabe(
-            id: Int, sprache: Sprachen, text: String
+            id: Int, sprache: Sprachen, bezeichnung: String
         ): Pair<Int, MutableMap<Sprachen, String>> {
+
+
+            /*
+            für alle Sprachen: Speichert die Bezeichnung sowohl
+                in der entsprechenden Sprache als auch in OG, oder setzt "".
+
+            Unterschied zu fromYaml(): die Map enthält Strings zu allen Sprachen, ggf. leere Strings
+            */
             val localizations =
-                Sprachen.entries.associateWith { if (it == sprache || it == Sprachen.OG) text else "" }
+                Sprachen.entries.associateWith { if (it == sprache || it == Sprachen.OG) bezeichnung else "" }
                     .toMutableMap()
+
             return id to localizations
         }
 
+        /**
+         * Erstellt ein Spielelement aus einem YAML-Abschnitt.
+         *
+         * @param data YAML-Abschnitt eines Elements
+         * @return Tupel (id, localizations) aka abstraktes Objekt von LokalisierbaresElement
+         */
         @Suppress("UNCHECKED_CAST")
         fun fromYaml(
             data: Map<String, Any>
         ): Pair<Int, MutableMap<Sprachen, String>> {
             val id = data[ID] as Int
+
+            /*
+            für alle ausgelesenen Sprachen: Speichert die entsprechende Übersetzung.
+
+            Unterschied zu fromEingabe():
+                die Map enthält nur die ausgelesenen Sprachen, auch wenn es mehr Enum-Einträge gibt.
+            */
             val localizations = ((data[TEXT]
                 ?: data[NAME]) as Map<String, String>).mapKeys { Sprachen.valueOf(it.key) }
                 .toMutableMap()
