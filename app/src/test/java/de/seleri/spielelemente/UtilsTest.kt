3076c98e8a8e30d472c0559493282e5f9041c26b
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-06-25 11:13:05 +0200

    Test hinzugefügt: ladeYamlDaten(pfad: String): Map<String, Any>

---
+++ app/src/test/java/de/seleri/spielelemente/UtilsTest.kt
@@ -0,0 +1,28 @@
+package de.seleri.spielelemente
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertThrows
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.yaml.snakeyaml.Yaml
+
+fun ladeYamlDaten(pfad: String): Map<String, Any> {
+    val inputStream = object{}.javaClass.classLoader?.getResourceAsStream(pfad)
+        ?: throw IllegalArgumentException("Datei nicht gefunden: $pfad")
+    return Yaml().load(inputStream)
+}
+
+class UtilsTest {
+
+    @Test
+    fun `ladeYamlDaten() Test-Dateien problemlos auslesen`() {
+        assertThrows(IllegalArgumentException::class.java)  {
+            ladeYamlDaten("nicht_existierend.yml")
+        }
+
+        val daten = ladeYamlDaten("UtilsTest.yml")
+        assertTrue(daten.isNotEmpty())
+        assertTrue(daten["Test"] as Boolean)
+    }
+}
