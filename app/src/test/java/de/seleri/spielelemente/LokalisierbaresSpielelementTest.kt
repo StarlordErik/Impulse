50e45f90d259f50bcfb365a448222a7e0e09617e
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-06-28 10:28:28 +0200

    null-Akzeptanz bei Localizations in der Dummy-Klasse implementiert

--- app/src/test/java/de/seleri/spielelemente/LokalisierbaresSpielelementTest.kt
+++ app/src/test/java/de/seleri/spielelemente/LokalisierbaresSpielelementTest.kt
@@ -2,7 +2,7 @@ package de.seleri.spielelemente
 
 /** Dummy-Implementierung, da LokalisierbaresSpielelement abstrakt ist */
 class DummyLokalisierbaresSpielelement(
-    override val id: Int, override val localizations: MutableMap<Sprachen, String>
+    override val id: Int, override val localizations: MutableMap<Sprachen, String?>
 ): LokalisierbaresSpielelement(id, localizations)
 
 class LokalisierbaresSpielelementTest {}
