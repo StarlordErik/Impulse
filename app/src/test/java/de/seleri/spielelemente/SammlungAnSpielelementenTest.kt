c503afe9fc65bff5bbae9d1c36e540abd0b9d35f
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-06-29 15:13:29 +0200

    SammlungAnSpielelementenTest angefangen: data class und Datenbank-Datei erstellt

---
+++ app/src/test/java/de/seleri/spielelemente/SammlungAnSpielelementenTest.kt
@@ -0,0 +1,37 @@
+package de.seleri.spielelemente
+
+data class DummySammlungAnSpielelementen(
+    override var id: Int,
+    override val localizations: MutableMap<Sprachen, String?>,
+    override val originaleElemente: Map<String, MutableSet<Karte>>,
+    override val hinzugefuegteElemente: MutableSet<Karte>
+): SammlungAnSpielelementen<Karte>(
+    id, localizations, originaleElemente, hinzugefuegteElemente
+) {
+    fun dummyOriginaleUndHinzugefuegteElementeToYaml(elemente: String): String =
+        originaleUndHinzugefuegteElementeToYaml(elemente)
+
+    override fun getKarten(): Set<Karte> = getAlleElemente()
+    override fun getAktuelleKarten(): Set<Karte> = getAlleAktuellenElemente()
+    override fun getUngeseheneKarten(): Set<Karte> = geseheneKartenRausfiltern(getAktuelleKarten())
+    override fun setKartenUngesehen() = setKartenUngesehen(getAktuelleKarten())
+
+    fun dummyElementeHinzufuegen(neueElemente: Collection<Karte>) =
+        elementeHinzufuegen(neueElemente)
+
+    fun dummyElementeEntfernen(element: Karte) = elementEntfernen(element)
+
+    companion object {
+        fun fromEingabe(
+            id: Int, sprache: Sprachen, name: String, originaleKarten: Collection<Karte>
+        ): DummySammlungAnSpielelementen =
+            fromEingabe(id, sprache, name, originaleKarten, ::DummySammlungAnSpielelementen)
+
+        fun fromYaml(yamlDaten: Map<String, Any>, moeglicheKarten: Collection<Karte>): DummySammlungAnSpielelementen =
+            fromYaml(yamlDaten, moeglicheKarten, ::DummySammlungAnSpielelementen)
+    }
+}
+
+
+
+class SammlungAnSpielelementenTest
