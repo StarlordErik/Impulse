33256c6e32b636b24bbb99d54985b25e9c8a728f
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-09 14:02:40 +0200

    Test zu `attributToYamlZeile()` für null, Integer, Boolean und String hinzugefügt

--- app/src/test/java/de/seleri/spielelemente/UtilsTest.kt
+++ app/src/test/java/de/seleri/spielelemente/UtilsTest.kt
@@ -1,24 +1,60 @@
 package de.seleri.spielelemente
 
-/** kürzere Schreibeweise von attributToYamlZeile() */
-fun atyz(anzahlEinrueckungen: Int, attributsname: String, attributswert: Any?) =
-    attributToYamlZeile(anzahlEinrueckungen, attributsname, attributswert)
-
-fun testAttribute(): Map<String, Any?> {
-    val collectionAnSpielelementen = setOf<LokalisierbaresSpielelement>(
-        // TODO dummy Objekte (in LokalisierbaresSpielementTest.kt erstellen)
-    ) as Collection<LokalisierbaresSpielelement>
-
-    // keys sind die Klassentypen der Attributstypen
-    return mapOf(
-        "String" to "String",
-        "Collection<LokalisierbaresSpielelement>" to collectionAnSpielelementen,
-        "Map<Sprache, String>" to mapOf(Sprachen.OG to "Test"),
-        "null" to null
-    )
-}
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class UtilsTest {
 
+    @Test
+    fun `attributToYamlZeile() null`() {
+        val anzahlEinrueckungen = 0
+        val attributsname = "null"
+        val attributswert = null
+
+        val actual = attributToYamlZeile(anzahlEinrueckungen, attributsname, attributswert)
+
+        val expected = "$attributsname:\n"
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `attributToYamlZeile() Integer`() {
+        val anzahlEinrueckungen = 1
+        val attributsname = "Integer"
+        val attributswert = 0
+
+        val actual = attributToYamlZeile(anzahlEinrueckungen, attributsname, attributswert)
+
+        val expected = "  $attributsname: $attributswert\n"
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `attributToYamlZeile() Boolean`() {
+        val anzahlEinrueckungen = 2
+        val attributsname = "Boolean"
+        val attributswert = true
+
+        val actual = attributToYamlZeile(anzahlEinrueckungen, attributsname, attributswert)
+
+        val expected = "    $attributsname: $attributswert\n"
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `attributToYamlZeile() String`() {
+        val anzahlEinrueckungen = 1
+        val attributsname = "String"
+        val attributswert =
+            "^1234567890ß´\tqwertzuiopü+\nasdfghjklöä#<yxcvbnm,.- °!\"§$%&" + //.
+                    "/()=?`QWERTZUIOPÜ*ASDFGHJKLÖÄ'>YXCVBNM;:_²³{[]}\\@€~|"
+
+        val actual = attributToYamlZeile(anzahlEinrueckungen, attributsname, attributswert)
+
+        val expectedAttributswert = "^1234567890ß´\\tqwertzuiopü+\\nasdfghjklöä#<yxcvbn" + //.
+                "m,.- °!\\\"§$%&/()=?`QWERTZUIOPÜ*ASDFGHJKLÖÄ'>YXCVBNM;:_²³{[]}\\\\@€~|"
+        val expected = "  $attributsname: \"$expectedAttributswert\"\n"
+        assertEquals(expected, actual)
+    }
 
 }
