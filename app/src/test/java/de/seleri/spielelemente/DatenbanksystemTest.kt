56af8ec2d6018b68246ae7035a76546feb578ae0
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-09 21:22:36 +0200

    Test `getRandomKartentext() zufaelliger Kartentext einer Kategorie ausgeben` hinzugefügt

--- app/src/test/java/de/seleri/spielelemente/DatenbanksystemTest.kt
+++ app/src/test/java/de/seleri/spielelemente/DatenbanksystemTest.kt
@@ -71,4 +71,26 @@ class DatenbanksystemTest {
         assertEquals(expected, actual)
     }
 
+    @Test
+    fun `getRandomKartentext() zufaelliger Kartentext einer Kategorie ausgeben`() {
+        val dbs = Datenbanksystem(tmpDatenbankDatei())
+        val dummyKategorie = dbs.kategorien.finde("dummyKategorie5")!!
+        val kartentexte1 = dummyKategorie.getUngeseheneKarten().map { it.localizations[Sprachen.OG]!! }.toMutableSet()
+        val kartentexte2 = kartentexte1.toList()
+
+        // Test 1: alle ungesehenen Karten werden ohne Duplikate ausgegeben
+        kartentexte2.forEach { _ -> // wir iterieren über eine Kopie, die in der Schleife nicht verändert wird
+            val randomKartentext = dbs.getRandomKartentext(dummyKategorie)
+            assertTrue(kartentexte1.contains(randomKartentext))
+            kartentexte1.remove(randomKartentext)
+        }
+        assertEquals(0, kartentexte1.size)
+
+        // Test 2: nachdem alle Karten bereits gesehen wurden, kann jede wieder gesehen werden
+        val randomKartentext = dbs.getRandomKartentext(dummyKategorie)
+        assertTrue(kartentexte2.contains(randomKartentext))
+    }
+
+
+
 }
