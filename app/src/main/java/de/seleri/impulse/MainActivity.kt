aa58a31934ec1a449e77a9b7d5ac6f7752eb0206
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-12 17:07:52 +0200

    Die App voll funktionsfähig mit Composables zum Laufen gebracht!

--- app/src/main/java/de/seleri/impulse/MainActivity.kt
+++ app/src/main/java/de/seleri/impulse/MainActivity.kt
@@ -3,27 +3,58 @@ package de.seleri.impulse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.navigation.NavHostController
+import de.seleri.spielelemente.Datenbanksystem
+import de.seleri.spielelemente.Spiel
+import de.seleri.spielelemente.Sprachen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val dbs = Datenbanksystem.generieren(this)
         setContent {
-            MyApp() // Dein Einstiegspunkt für Compose UI
+            Navigation(dbs)
         }
     }
 }
 
 @Composable
-fun MyApp() {
-    MaterialTheme {
-        Greeting("Welt")
+fun StartScreen(navController: NavHostController, dbs: Datenbanksystem) {
+    Column{
+        Text(text = "Impulse")
+
+        dbs.spiele.forEach { spiel ->
+            Button(
+                onClick = {
+                    navController.navigate(Screen.Spiel.mitArgumenten(spiel.id))
+                }
+            ) {
+                Text(text = spiel.localizations[Sprachen.OG]!!)
+            }
+        }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hallo, $name!")
+fun SpielScreen(dbs: Datenbanksystem, spiel: Spiel) {
+    Column{
+        spiel.getAktuelleKategorien().forEach { kategorie ->
+            var kartentext by remember { mutableStateOf(kategorie.localizations[Sprachen.OG]!!) }
+            Button(
+                onClick = {
+                    kartentext = dbs.getRandomKartentext(kategorie)
+                }
+            ) {
+                Text(text = kartentext)
+            }
+        }
+    }
 }
