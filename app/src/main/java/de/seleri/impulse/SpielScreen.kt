7ddbae57dacb01c19874f55e57d53893f4cf00ec
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-12 21:49:12 +0200

    Kategorien und ihre Karten werden nun angezeigt

--- app/src/main/java/de/seleri/impulse/SpielScreen.kt
+++ app/src/main/java/de/seleri/impulse/SpielScreen.kt
@@ -1,28 +1,67 @@
 package de.seleri.impulse
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import de.seleri.spielelemente.Datenbanksystem
 import de.seleri.spielelemente.Spiel
 import de.seleri.spielelemente.Sprachen
 
 @Composable
 fun SpielScreen(dbs: Datenbanksystem, spiel: Spiel) {
-    Column {
-        spiel.getAktuelleKategorien().forEach { kategorie ->
-            var kartentext by remember { mutableStateOf(kategorie.localizations[Sprachen.OG]!!) }
-            Card (
+    val kartentexte = remember {
+        mutableStateMapOf<Int, String>()
+    }
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        verticalArrangement = Arrangement.Center
+    ) {
+        items(spiel.getAktuelleKategorien().toList(), key = { it.id }) { kategorie ->
+            val initialText = kategorie.localizations[Sprachen.OG]!!
+            val kartentext = kartentexte[kategorie.id] ?: initialText
+
+            Card(
                 onClick = {
-                    kartentext = dbs.getRandomKartentext(kategorie)
-                }) {
-                Text(text = kartentext)
+                    kartentexte[kategorie.id] = dbs.getRandomKartentext(kategorie)
+                },
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.secondaryContainer
+                ),
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 30.dp, vertical = 10.dp),
+                elevation = CardDefaults.cardElevation(4.dp)
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = kartentext,
+                        color = MaterialTheme.colorScheme.onSecondaryContainer,
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier.padding(15.dp)
+                    )
+                }
             }
         }
     }
