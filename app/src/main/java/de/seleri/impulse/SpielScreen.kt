0694a02d3c648fcd1bd8b92d45278bf4815e751e
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-12 22:03:40 +0200

    KategorieButton als Composable ausgelagert

--- app/src/main/java/de/seleri/impulse/SpielScreen.kt
+++ app/src/main/java/de/seleri/impulse/SpielScreen.kt
@@ -18,16 +18,16 @@ import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import de.seleri.spielelemente.Datenbanksystem
 import de.seleri.spielelemente.Spiel
 import de.seleri.spielelemente.Sprachen
+import kotlin.collections.set
 
 @Composable
 fun SpielScreen(dbs: Datenbanksystem, spiel: Spiel) {
-    val kartentexte = remember {
-        mutableStateMapOf<Int, String>()
-    }
+    val kartentexte = remember { mutableStateMapOf<Int, String>() }
 
     LazyColumn(
         modifier = Modifier
@@ -39,30 +39,42 @@ fun SpielScreen(dbs: Datenbanksystem, spiel: Spiel) {
             val initialText = kategorie.localizations[Sprachen.OG]!!
             val kartentext = kartentexte[kategorie.id] ?: initialText
 
-            Card(
-                onClick = {
+            KategorieButton(
+                kartentext = kartentext,
+                onCardClick = {
                     kartentexte[kategorie.id] = dbs.getRandomKartentext(kategorie)
-                },
-                colors = CardDefaults.cardColors(
-                    containerColor = MaterialTheme.colorScheme.secondaryContainer
-                ),
-                shape = RoundedCornerShape(12.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 30.dp, vertical = 10.dp),
-                elevation = CardDefaults.cardElevation(4.dp)
-            ) {
-                Box(
-                    modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center
-                ) {
-                    Text(
-                        text = kartentext,
-                        color = MaterialTheme.colorScheme.onSecondaryContainer,
-                        style = MaterialTheme.typography.titleLarge,
-                        modifier = Modifier.padding(15.dp)
-                    )
                 }
-            }
+            )
+        }
+    }
+}
+
+@Composable
+fun KategorieButton(
+    kartentext: String,
+    onCardClick: () -> Unit
+) {
+    Card(
+        onClick = onCardClick,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer
+        ),
+        shape = RoundedCornerShape(12.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 30.dp, vertical = 10.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = kartentext,
+                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(15.dp)
+            )
         }
     }
 }
