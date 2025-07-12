53d5e067388c3911518f7522f011d2f2af393d6c
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-12 20:02:26 +0200

    Titel wie im Original angezeigt

--- app/src/main/java/de/seleri/impulse/StartScreen.kt
+++ app/src/main/java/de/seleri/impulse/StartScreen.kt
@@ -1,10 +1,20 @@
 package de.seleri.impulse
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalWindowInfo
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import de.seleri.spielelemente.Datenbanksystem
 import de.seleri.spielelemente.Sprachen
@@ -12,7 +22,7 @@ import de.seleri.spielelemente.Sprachen
 @Composable
 fun StartScreen(navController: NavHostController, dbs: Datenbanksystem) {
     Column {
-        Text(text = "Impulse")
+        Titel()
 
         dbs.spiele.forEach { spiel ->
             Card(
@@ -25,3 +35,31 @@ fun StartScreen(navController: NavHostController, dbs: Datenbanksystem) {
         }
     }
 }
+
+@Preview
+@Composable
+fun Titel() {
+    val windowInfo = LocalWindowInfo.current
+    val density = LocalDensity.current
+    val containerHeightDp = windowInfo.containerSize.height
+
+    // Umrechnung von px in dp
+    val containerHeight = with(density) { containerHeightDp.toDp() }
+    val anteilDesTitelsAmBildschirm = 3.toFloat()
+    val titelHoehe = containerHeight / anteilDesTitelsAmBildschirm
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(titelHoehe),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Impulse",
+            color = MaterialTheme.colorScheme.primary,
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
