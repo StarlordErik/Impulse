93eac640f9b1df7557641708243358add9eadd1a
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-13 14:14:40 +0200

    `MainActivity` mit Hilt annotieren

--- app/src/main/java/de/seleri/impulse/MainActivity.kt
+++ app/src/main/java/de/seleri/impulse/MainActivity.kt
@@ -4,20 +4,28 @@ import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.HiltAndroidApp
 import de.seleri.spielelemente.Datenbanksystem
+import javax.inject.Inject
 
+
+@AndroidEntryPoint
 class MainActivity: ComponentActivity() {
+    @Inject
+    lateinit var dbs: Datenbanksystem
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val dbs = Datenbanksystem.generieren(this)
         setContent {
-            ImpulseTheme{
+            ImpulseTheme {
                 Navigation(dbs)
             }
         }
     }
 }
 
+
 @HiltAndroidApp
-class Impulse : Application()
+class Impulse: Application()
+
