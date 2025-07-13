0d685236b0764abadf9eb85e8a7442f5ec0fbebd
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-13 16:14:23 +0200

    ungenutzter Import entfernt

--- app/src/test/java/de/seleri/spielelemente/DatenbanksystemTest.kt
+++ app/src/test/java/de/seleri/spielelemente/DatenbanksystemTest.kt
@@ -1,6 +1,5 @@
 package de.seleri.spielelemente
 
-import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
