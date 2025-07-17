105752d0ad3ae3cf31a11c2b760b9ed4e1d843a0
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-18 00:48:03 +0200

    Dao erstellt: von `Spiel`

--- kern/daten/src/main/java/de/seleri/kern/daten/daos/Spiel.kt
+++ kern/daten/src/main/java/de/seleri/kern/daten/daos/Spiel.kt
@@ -1,3 +1,26 @@
 package de.seleri.kern.daten.daos
 
-class Spiel {}
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import de.seleri.kern.daten.entities.Spiel
+
+@Dao
+interface Spiel {
+
+  @Upsert
+  suspend fun upsert(spiel: Spiel)
+
+  @Delete
+  suspend fun delete(spiel: Spiel)
+
+  @Query("SELECT * FROM Spiele WHERE id = :id")
+  suspend fun getByID(id: Int): Spiel?
+
+  @Query("SELECT * FROM Spiele")
+  suspend fun getAlle(): List<Spiel>
+
+  @Query("SELECT * FROM Spiele WHERE ausgeblendet = 0")
+  suspend fun getAlleSichtbaren(): List<Spiel>
+}
