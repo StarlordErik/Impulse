dc9d772c03304d923b63fb39f32109177894e24f
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-18 01:11:37 +0200

    Dao erstellt: von `Kartentext`

---
+++ kern/daten/src/main/java/de/seleri/kern/daten/daos/Kartentext.kt
@@ -0,0 +1,29 @@
+package de.seleri.kern.daten.daos
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import de.seleri.kern.daten.entities.Kartentext
+
+@Dao
+interface Kartentext {
+
+  @Upsert
+  suspend fun upsert(kartentext: Kartentext)
+
+  @Delete
+  suspend fun delete(kartentext: Kartentext)
+
+  @Query("SELECT * FROM Spiele WHERE id = :id")
+  suspend fun getByID(id: Int): Kartentext?
+
+  @Query("SELECT * FROM Spiele")
+  suspend fun getAlle(): List<Kartentext>
+
+  @Query("SELECT * FROM Spiele WHERE inaktiv = 0")
+  suspend fun getAktive(): List<Kartentext>
+
+  @Query("SELECT * FROM Kartentexte WHERE inaktiv = 0 AND gesehen = 0")
+  suspend fun getUngesehene(): List<Kartentext>
+}
