f27341d492fd30182af7e156b03954ba588f222b
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-18 02:43:59 +0200

    SpielXKategorie als Dao umgesetzt

--- kern/daten/src/main/java/de/seleri/kern/daten/daos/SpielDao.kt
+++ kern/daten/src/main/java/de/seleri/kern/daten/daos/SpielDao.kt
@@ -3,8 +3,10 @@ package de.seleri.kern.daten.daos
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Upsert
 import de.seleri.kern.daten.entities.Spiel
+import de.seleri.kern.daten.relationen.SpielMitKategorien
 
 @Dao
 interface SpielDao {
@@ -23,4 +25,19 @@ interface SpielDao {
 
   @Query("SELECT * FROM Spiele WHERE inaktiv = 0")
   suspend fun getAktive(): List<Spiel>
+
+  @Transaction
+  @Query("SELECT * FROM Spiele WHERE id = :spielId")
+  fun getMitKategorien(spielId: Int): SpielMitKategorien
+
+  @Transaction
+  @Query(
+    """
+  SELECT DISTINCT s.* FROM Spiele s
+  INNER JOIN SpielXKategorie x ON s.id = x.spielID
+  INNER JOIN Kategorien k ON x.kategorieID = k.id
+  WHERE s.inaktiv = 0 AND k.inaktiv = 0
+"""
+  )
+  fun getMitAktivenKategorien(): List<SpielMitKategorien>
 }
