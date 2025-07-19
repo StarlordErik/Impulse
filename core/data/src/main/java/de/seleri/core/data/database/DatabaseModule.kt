6664e12f8af5daeddc124b545d6f86f6001ffcc5
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-19 23:28:46 +0200

    `DatabaseModule` erstellt

---
+++ core/data/src/main/java/de/seleri/core/data/database/DatabaseModule.kt
@@ -0,0 +1,48 @@
+package de.seleri.core.data.database
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import de.seleri.core.data.daos.KartentextDao
+import de.seleri.core.data.daos.KategorieDao
+import de.seleri.core.data.daos.LokalisierungDao
+import de.seleri.core.data.daos.SpielDao
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+	@Provides
+	@Singleton
+	fun provideDatabase(
+		@ApplicationContext
+		context: Context
+	): AppDatabase =
+		Room
+			.databaseBuilder(
+				context, AppDatabase::class.java, "app_db"
+			)
+			.build()
+
+	@Provides
+	fun provideLokalisierungDao(db: AppDatabase): LokalisierungDao =
+		db.lokalisierungDao()
+
+	@Provides
+	fun provideKartentextDao(db: AppDatabase): KartentextDao =
+		db.kartentextDao()
+
+	@Provides
+	fun provideKategorieDao(db: AppDatabase): KategorieDao =
+		db.kategorieDao()
+
+	@Provides
+	fun provideSpielDao(db: AppDatabase): SpielDao =
+		db.spielDao()
+}
+
