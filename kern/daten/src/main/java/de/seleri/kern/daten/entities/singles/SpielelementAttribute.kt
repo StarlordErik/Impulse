e9a2d72b38c4e7fa0d031b267adb7490932c6d31
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-19 18:38:01 +0200

    ogSprache: In welcher Sprache wurde das Spielelelement verfasst?

--- kern/daten/src/main/java/de/seleri/kern/daten/entities/singles/SpielelementAttribute.kt
+++ kern/daten/src/main/java/de/seleri/kern/daten/entities/singles/SpielelementAttribute.kt
@@ -1,6 +1,9 @@
 package de.seleri.kern.daten.entities.singles
 
+import de.seleri.kern.Sprache
+
 data class SpielelementAttribute(
+  val ogSprache: Sprache = Sprache.DE,
   val inaktiv: Boolean = false,
   val selbstErstellt: Boolean = false,
 )
