bf98d4b7eafddcdd32b646eaa906b50674b5da9e
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-20 05:14:53 +0200

    `LokalisierungRepo` angepasst an das Nutzen des expliziten Fremdschlüssel-Typ

--- core/domain/src/main/java/de/seleri/core/domain/repositories/LokalisierungRepo.kt
+++ core/domain/src/main/java/de/seleri/core/domain/repositories/LokalisierungRepo.kt
@@ -1,3 +1,12 @@
 package de.seleri.core.domain.repositories
 
-interface LokalisierungRepo {}
+import de.seleri.core.domain.model.Lokalisierung
+
+interface LokalisierungRepo {
+
+	suspend fun upsert(lokalisierung: Lokalisierung)
+	suspend fun delete(lokalisierung: Lokalisierung)
+
+	suspend fun getById(id: Int): Lokalisierung?
+	suspend fun getBearbeitete(): List<Lokalisierung>
+}
