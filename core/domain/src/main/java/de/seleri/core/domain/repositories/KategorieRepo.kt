19245f2b654458565c1dae47dd875f8a0a1f689c
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-20 18:06:21 +0200

    NzuMRepo implementiert

--- core/domain/src/main/java/de/seleri/core/domain/repositories/KategorieRepo.kt
+++ core/domain/src/main/java/de/seleri/core/domain/repositories/KategorieRepo.kt
@@ -1,5 +1,6 @@
 package de.seleri.core.domain.repositories
 
+import de.seleri.core.domain.model.spielelemente.Kartentext
 import de.seleri.core.domain.model.spielelemente.Kategorie
 
-interface KategorieRepo: DatenbankObjektRepo<Kategorie>, BestandteilRepo<Kategorie>
+interface KategorieRepo: DatenbankObjektRepo<Kategorie>, BestandteilRepo<Kategorie>, NzuMRepo<Kategorie, Kartentext>
--- core/domain/src/main/java/de/seleri/core/domain/repositories/SpielRepo.kt
+++ core/domain/src/main/java/de/seleri/core/domain/repositories/SpielRepo.kt
@@ -1,5 +1,6 @@
 package de.seleri.core.domain.repositories
 
+import de.seleri.core.domain.model.spielelemente.Kategorie
 import de.seleri.core.domain.model.spielelemente.Spiel
 
-interface SpielRepo: DatenbankObjektRepo<Spiel>
+interface SpielRepo: DatenbankObjektRepo<Spiel>, NzuMRepo<Spiel, Kategorie>
