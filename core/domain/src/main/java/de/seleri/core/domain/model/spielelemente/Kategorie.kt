aa94273e5142ff1cff731dd8b68b77c6b6c69f72
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-20 03:25:21 +0200

    `Kategorie` ist nun eine `Sammlung`!

--- core/domain/src/main/java/de/seleri/core/domain/model/spielelemente/Kategorie.kt
+++ core/domain/src/main/java/de/seleri/core/domain/model/spielelemente/Kategorie.kt
@@ -3,10 +3,18 @@ package de.seleri.core.domain.model.spielelemente
 import de.seleri.core.domain.model.Spielelement
 import de.seleri.core.domain.model.SpielelementBasis
 import de.seleri.core.domain.model.spielelemente.sammlungen.Bestandteil
+import de.seleri.core.domain.model.spielelemente.sammlungen.Sammlung
+import de.seleri.core.domain.model.spielelemente.sammlungen.SammlungsBestandteile
 
 data class Kategorie(
 	val spielelementBasis: SpielelementBasis,
-): Spielelement by spielelementBasis, Bestandteil {
+
+	val sammlungsBestandteile: SammlungsBestandteile<Kartentext>,
+): Spielelement by spielelementBasis, Sammlung<Kartentext> by sammlungsBestandteile, Bestandteil {
+
+	override fun karte(texteProKarte: Int): List<Kartentext> {
+		TODO("Not yet implemented")
+	}
 
 	override fun wurdeGesehen() {
 		TODO("Not yet implemented")
