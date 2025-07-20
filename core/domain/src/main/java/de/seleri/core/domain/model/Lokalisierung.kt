dc58c4397f2e21687c6746cf19b59a8076a7cb11
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-20 02:32:58 +0200

    `Lokalisierung` erstellt

--- core/domain/src/main/java/de/seleri/core/domain/model/Lokalisierung.kt
+++ core/domain/src/main/java/de/seleri/core/domain/model/Lokalisierung.kt
@@ -1,3 +1,18 @@
 package de.seleri.core.domain.model
 
-data class Lokalisierung {}
+import de.seleri.core.common.Sprache
+
+data class Lokalisierung(
+	val datenbankEintrag: DatenbankEintrag,
+
+	val bezeichnung: String,
+	val sprache: Sprache,
+	val bearbeitet: Boolean,
+
+	val spielelementID: Int,
+): DatenbankEintrag by datenbankEintrag, Comparable<Lokalisierung> {
+
+	override fun compareTo(other: Lokalisierung): Int {
+		TODO("Not yet implemented")
+	}
+}
