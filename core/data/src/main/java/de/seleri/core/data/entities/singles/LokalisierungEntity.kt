9076933fa1c9f5af3b5fa262d2957f92a3afbcc6
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-20 18:19:35 +0200

    Lokalisierung vereinfacht

--- core/data/src/main/java/de/seleri/core/data/entities/singles/LokalisierungEntity.kt
+++ core/data/src/main/java/de/seleri/core/data/entities/singles/LokalisierungEntity.kt
@@ -1,28 +1,11 @@
 package de.seleri.core.data.entities.singles
 
 import androidx.room.Entity
-import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import de.seleri.core.common.LokalisierungVon
 import de.seleri.core.common.Sprache
 
-@Entity(
-	tableName = "Lokalisierungen", foreignKeys = [ForeignKey(
-		entity = SpielEntity::class,
-		parentColumns = ["id"],
-		childColumns = ["spielId"],
-		onDelete = ForeignKey.Companion.CASCADE
-	), ForeignKey(
-		entity = KategorieEntity::class,
-		parentColumns = ["id"],
-		childColumns = ["kategorieId"],
-		onDelete = ForeignKey.Companion.CASCADE
-	), ForeignKey(
-		entity = KartentextEntity::class,
-		parentColumns = ["id"],
-		childColumns = ["kartentextId"],
-		onDelete = ForeignKey.Companion.CASCADE
-	)]
-)
+@Entity(tableName = "Lokalisierungen")
 data class LokalisierungEntity(
 	@PrimaryKey(autoGenerate = true)
 	val id: Int,
@@ -31,7 +14,6 @@ data class LokalisierungEntity(
 	val sprache: Sprache = Sprache.OG,
 	val bearbeitet: Boolean = false,
 
-	val spielID: Int? = null,
-	val kategorieID: Int? = null,
-	val kartentextID: Int? = null,
+	val lokalisierungVon: LokalisierungVon,
+	val spielelementID: Int,
 )
