7254b16c2b0dbe72bce3e8ceb3545afba0cea46f
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-20 21:54:36 +0200

    `KartentextMapper` erstellt!

--- core/domain/src/main/java/de/seleri/core/domain/repositories/LokalisierungRepo.kt
+++ core/domain/src/main/java/de/seleri/core/domain/repositories/LokalisierungRepo.kt
@@ -1,16 +1,19 @@
 package de.seleri.core.domain.repositories
 
-import de.seleri.core.common.Sprache
 import de.seleri.core.domain.model.Lokalisierung
+import de.seleri.core.domain.model.ids.BestandteilID
 import de.seleri.core.domain.model.ids.SpielelementID
 import de.seleri.core.domain.repositories.basis.BestandteilRepo
 import de.seleri.core.domain.repositories.basis.DatenbankObjektRepo
 
 interface LokalisierungRepo: DatenbankObjektRepo<Lokalisierung>, BestandteilRepo<Lokalisierung> {
 
-	suspend fun getForSpielelementInSprache(spielelementID: SpielelementID, sprache: Sprache): Lokalisierung
-	suspend fun getForSpielelementeInSprache(spielelementID: Collection<SpielelementID>): List<Lokalisierung>
+	suspend fun getLokalisierungIDsForSpielelement(spielelementID: SpielelementID): List<BestandteilID.LokalisierungID>
 
-	suspend fun upsertForSpielelementInSprache(spielelementID: SpielelementID, sprache: Sprache)
+	/*
+		suspend fun getForSpielelementInSprache(spielelementID: SpielelementID, sprache: Sprache): Lokalisierung
+		suspend fun getForSpielelementeInSprache(spielelementID: Collection<SpielelementID>): List<Lokalisierung>
 
+		suspend fun upsertForSpielelementInSprache(spielelementID: SpielelementID, sprache: Sprache)
+	*/
 }
--- core/repository/src/main/java/de/seleri/core/repository/mapper/KartentextMapper.kt
+++ core/repository/src/main/java/de/seleri/core/repository/mapper/KartentextMapper.kt
@@ -2,32 +2,43 @@ package de.seleri.core.repository.mapper
 
 import de.seleri.core.data.entities.singles.KartentextEntity
 import de.seleri.core.data.entities.singles.SpielelementBasis
-import de.seleri.core.domain.model.Lokalisierung
+import de.seleri.core.domain.model.DatenbankObjektDaten
+import de.seleri.core.domain.model.ids.BestandteilID
 import de.seleri.core.domain.model.spielelemente.Kartentext
 import de.seleri.core.domain.model.spielelemente.SpielelementDaten
 
-fun KartentextEntity.toKartentext(lokalisierungen: Collection<Lokalisierung>): Kartentext {
-	val spielelementDaten = object: SpielelementDaten(
-		id = this.id,
-		lokalisierungen = lokalisierungen,
-		ogSprache = spielelementBasis.ogSprache,
-		selbstErstellt = spielelementBasis.selbstErstellt,
-		inaktiv = spielelementBasis.inaktiv,
-		favorisiert = spielelementBasis.favorisiert
-	) {}
 
-	return Kartentext(
-		spielelementDaten = spielelementDaten, gesehen = this.gesehen, besprochen = this.besprochen
+fun KartentextEntity.toKartentext(
+	lokalisierungIds: List<BestandteilID.LokalisierungID>
+): Kartentext =
+	// @formatter:off
+	Kartentext(
+		spielelementDaten = SpielelementDaten(
+			datenbankObjektDaten = DatenbankObjektDaten(
+				id = id
+			),
+			lokalisierungen = lokalisierungIds,
+			ogSprache = spielelementBasis.ogSprache,
+			selbstErstellt = spielelementBasis.selbstErstellt,
+			inaktiv = spielelementBasis.inaktiv,
+			favorisiert = spielelementBasis.favorisiert
+		),
+		gesehen = gesehen,
+		besprochen = besprochen
 	)
-}
+	// @formatter:on
 
-fun Kartentext.toEntity(): KartentextEntity {
-	return KartentextEntity(
-		id = this.id, spielelementBasis = SpielelementBasis(
+fun Kartentext.toEntity(): KartentextEntity =
+	// @formatter:off
+	KartentextEntity(
+		id = this.id,
+		spielelementBasis = SpielelementBasis(
 			ogSprache = this.spielelementDaten.ogSprache,
 			selbstErstellt = this.spielelementDaten.selbstErstellt,
 			inaktiv = this.spielelementDaten.inaktiv,
 			favorisiert = this.spielelementDaten.favorisiert
-		), gesehen = this.gesehen, besprochen = this.besprochen
+		),
+		gesehen = this.gesehen,
+		besprochen = this.besprochen
 	)
-}
+	// @formatter:on
