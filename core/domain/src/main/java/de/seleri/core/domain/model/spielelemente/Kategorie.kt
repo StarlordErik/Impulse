738988da462a997a10bc14998b9a38fabf7b98dc
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-20 03:21:19 +0200

    `Kategorie` ist nun ein `Bestandteil`!

--- core/domain/src/main/java/de/seleri/core/domain/model/spielelemente/Kategorie.kt
+++ core/domain/src/main/java/de/seleri/core/domain/model/spielelemente/Kategorie.kt
@@ -2,9 +2,13 @@ package de.seleri.core.domain.model.spielelemente
 
 import de.seleri.core.domain.model.Spielelement
 import de.seleri.core.domain.model.SpielelementBasis
+import de.seleri.core.domain.model.spielelemente.sammlungen.Bestandteil
 
 data class Kategorie(
 	val spielelementBasis: SpielelementBasis,
-): Spielelement by spielelementBasis {
+): Spielelement by spielelementBasis, Bestandteil {
 
+	override fun wurdeGesehen() {
+		TODO("Not yet implemented")
+	}
 }
