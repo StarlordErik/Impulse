a9aecd6d5bc98c9eb5a9bfee576b6b3b61087b6a
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-21 01:02:45 +0200

    `GetAlleSpieleCase` --> `GetAlleSpieleUsecase`

--- core/domain/src/main/java/de/seleri/core/domain/usecases/GetAlleSpieleUsecase.kt
+++ core/domain/src/main/java/de/seleri/core/domain/usecases/GetAlleSpieleUsecase.kt
@@ -3,7 +3,7 @@ package de.seleri.core.domain.usecases
 import de.seleri.core.domain.model.spielelemente.Spiel
 import de.seleri.core.domain.repositories.SpielRepo
 
-class GetAlleSpieleCase(private val spielRepo: SpielRepo) {
+class GetAlleSpieleUsecase(private val spielRepo: SpielRepo) {
 
 	suspend operator fun invoke(): List<Spiel> {
 		return spielRepo.getAlleSpiele()
