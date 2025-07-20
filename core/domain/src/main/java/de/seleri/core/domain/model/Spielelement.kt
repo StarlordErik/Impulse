a5066f256e0ec3238b1a2a133bdbaceb18e8456a
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-20 12:58:48 +0200

    Lokalisierungen werden nur als Int gespeichert

--- core/domain/src/main/java/de/seleri/core/domain/model/Spielelement.kt
+++ core/domain/src/main/java/de/seleri/core/domain/model/Spielelement.kt
@@ -1,7 +1,8 @@
 package de.seleri.core.domain.model
 
 interface Spielelement: DatenbankEintrag, Comparable<Spielelement> {
-	val lokalisierungen: Collection<Lokalisierung>
+
+	val lokalisierungen: Collection<Int>
 
 	override fun compareTo(other: Spielelement): Int {
 		TODO("Not yet implemented")
--- core/domain/src/main/java/de/seleri/core/domain/model/SpielelementDaten.kt
+++ core/domain/src/main/java/de/seleri/core/domain/model/SpielelementDaten.kt
@@ -4,7 +4,7 @@ import de.seleri.core.common.Sprache
 
 abstract class SpielelementDaten(
 	override val id: Int,
-	override val lokalisierungen: Collection<Lokalisierung>,
+	override val lokalisierungen: Collection<Int>,
 
 	val ogSprache: Sprache,
 	val selbstErstellt: Boolean,
