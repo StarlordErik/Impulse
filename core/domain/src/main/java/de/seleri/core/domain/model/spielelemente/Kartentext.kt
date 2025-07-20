91761ddaea72fdc16e23c7105fe33d6fb6da2c78
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-20 03:20:53 +0200

    `Kartentext` ist nun ein `Bestandteil`!

--- core/domain/src/main/java/de/seleri/core/domain/model/spielelemente/Kartentext.kt
+++ core/domain/src/main/java/de/seleri/core/domain/model/spielelemente/Kartentext.kt
@@ -2,12 +2,16 @@ package de.seleri.core.domain.model.spielelemente
 
 import de.seleri.core.domain.model.Spielelement
 import de.seleri.core.domain.model.SpielelementBasis
+import de.seleri.core.domain.model.spielelemente.sammlungen.Bestandteil
 
 data class Kartentext(
 	val spielelementBasis: SpielelementBasis,
 
 	val gesehen: Boolean,
 	val besprochen: Boolean,
-): Spielelement by spielelementBasis {
+): Spielelement by spielelementBasis, Bestandteil {
 
+	override fun wurdeGesehen() {
+		TODO("Not yet implemented")
+	}
 }
