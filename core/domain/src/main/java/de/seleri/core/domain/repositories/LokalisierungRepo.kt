4a4034d51ea324e71b36cb401bcedb283ab965b4
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-21 01:03:13 +0200

    `GetTextUsecase` erstellt!

--- core/domain/src/main/java/de/seleri/core/domain/repositories/LokalisierungRepo.kt
+++ core/domain/src/main/java/de/seleri/core/domain/repositories/LokalisierungRepo.kt
@@ -1,5 +1,6 @@
 package de.seleri.core.domain.repositories
 
+import de.seleri.core.common.Sprache
 import de.seleri.core.domain.model.Lokalisierung
 import de.seleri.core.domain.model.ids.BestandteilID
 import de.seleri.core.domain.model.ids.SpielelementID
@@ -10,10 +11,5 @@ interface LokalisierungRepo: DatenbankObjektRepo<Lokalisierung>, BestandteilRepo
 
 	suspend fun getLokalisierungIDsForSpielelement(spielelementID: SpielelementID): List<BestandteilID.LokalisierungID>
 
-	/*
-		suspend fun getForSpielelementInSprache(spielelementID: SpielelementID, sprache: Sprache): Lokalisierung
-		suspend fun getForSpielelementeInSprache(spielelementID: Collection<SpielelementID>): List<Lokalisierung>
-
-		suspend fun upsertForSpielelementInSprache(spielelementID: SpielelementID, sprache: Sprache)
-	*/
+	suspend fun getByIDinSprache(spielelementID: SpielelementID, inSprache: Sprache): Lokalisierung?
 }
---
+++ core/domain/src/main/java/de/seleri/core/domain/usecases/GetTextUsecase.kt
@@ -0,0 +1,17 @@
+package de.seleri.core.domain.usecases
+
+import de.seleri.core.common.Sprache
+import de.seleri.core.domain.model.ids.SpielelementID
+import de.seleri.core.domain.repositories.LokalisierungRepo
+
+class GetTextUsecase(
+	private val lokalisierungRepo: LokalisierungRepo, private val fallbackSprache: Sprache = Sprache.OG
+) {
+
+	suspend operator fun invoke(spielelementID: SpielelementID, inSprache: Sprache): String {
+		val lokalisierung = lokalisierungRepo.getByIDinSprache(spielelementID, inSprache)
+			?: lokalisierungRepo.getByIDinSprache(spielelementID, fallbackSprache)!!
+
+		return lokalisierung.bezeichnung
+	}
+}
