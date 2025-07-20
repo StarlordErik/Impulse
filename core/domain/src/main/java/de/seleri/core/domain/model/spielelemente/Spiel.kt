c1197786f29380520c4d8124068e79bdcea4603d
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-20 03:23:59 +0200

    `Spiel` ist nun eine `Sammlung`!

--- core/domain/src/main/java/de/seleri/core/domain/model/spielelemente/Spiel.kt
+++ core/domain/src/main/java/de/seleri/core/domain/model/spielelemente/Spiel.kt
@@ -2,12 +2,19 @@ package de.seleri.core.domain.model.spielelemente
 
 import de.seleri.core.domain.model.Spielelement
 import de.seleri.core.domain.model.SpielelementBasis
+import de.seleri.core.domain.model.spielelemente.sammlungen.Sammlung
+import de.seleri.core.domain.model.spielelemente.sammlungen.SammlungsBestandteile
 
 data class Spiel(
 	val spielelementBasis: SpielelementBasis,
 
+	val sammlungsBestandteile: SammlungsBestandteile<Kategorie>,
+
 	val texteProKarte: Int,
 	val bildDateiname: String?,
-): Spielelement by spielelementBasis {
+): Spielelement by spielelementBasis, Sammlung<Kategorie> by sammlungsBestandteile {
 
+	override fun karte(texteProKarte: Int): List<Kartentext> {
+		TODO("Not yet implemented")
+	}
 }
