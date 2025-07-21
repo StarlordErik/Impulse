ebd72b5103430ed5a2e71742f8fccd8216294459
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-21 19:51:55 +0200

    Upsert zukunftstauglich gemacht

--- core/domain/src/main/java/de/seleri/core/domain/repositories/basis/SpielelementRepo.kt
+++ core/domain/src/main/java/de/seleri/core/domain/repositories/basis/SpielelementRepo.kt
@@ -4,6 +4,9 @@ import de.seleri.core.domain.model.spielelemente.Spielelement
 
 interface SpielelementRepo<S: Spielelement> {
 
-	suspend fun upsert(spielelement: S)
+	/**
+	 * @return generierte ID oder null bei nur-Update
+	 */
+	suspend fun upsert(spielelement: S): Integer?
 	suspend fun delete(spielelement: S)
 }
