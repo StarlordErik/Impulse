88bb0fbb10f78cb9dc7512ee58f32f59a1c55abb
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-21 22:03:55 +0200

    Umbennenung

--- core/domain/src/main/java/de/seleri/core/domain/repositories/basis/SammlungRepo.kt
+++ core/domain/src/main/java/de/seleri/core/domain/repositories/basis/SammlungRepo.kt
@@ -5,6 +5,6 @@ import de.seleri.core.domain.model.spielelemente.sammlungen.Sammlung
 
 interface SammlungRepo<B: Bestandteil, S: Sammlung<B>> {
 
-	suspend fun insertVerbindung(sammlung: S, bestandteil: B)
-	suspend fun deleteVerbindung(sammlung: S, bestandteil: B)
+	suspend fun insertConnection(sammlung: S, bestandteil: B)
+	suspend fun deleteConnection(sammlung: S, bestandteil: B)
 }
