4f6ec3449fb4ed4a735c0d51d902a94d67afc05b
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-21 22:16:09 +0200

    SpielelementID-Type-Sicherheit implementiert!

--- core/domain/src/main/java/de/seleri/core/domain/repositories/LokalisierungRepo.kt
+++ core/domain/src/main/java/de/seleri/core/domain/repositories/LokalisierungRepo.kt
@@ -1,15 +1,16 @@
 package de.seleri.core.domain.repositories
 
+import de.seleri.core.common.idTypes.SpielelementID
 import de.seleri.core.domain.model.Lokalisierung
 
 interface LokalisierungRepo {
 
-	suspend fun upsertForSpiel(spielID: Int, lokalisierung: Lokalisierung)
-	suspend fun upsertForKategorie(kategorieID: Int, lokalisierung: Lokalisierung)
-	suspend fun upsertForKartentext(kartentextID: Int, lokalisierung: Lokalisierung)
+	suspend fun upsertForSpiel(spielID: SpielelementID.SpielID, lokalisierung: Lokalisierung)
+	suspend fun upsertForKategorie(kategorieID: SpielelementID.KategorieID, lokalisierung: Lokalisierung)
+	suspend fun upsertForKartentext(kartentextID: SpielelementID.KartentextID, lokalisierung: Lokalisierung)
 	suspend fun delete(lokalisierung: Lokalisierung)
 
-	suspend fun getForSpiel(spielID: Int): Collection<Lokalisierung>
-	suspend fun getForKategorie(kategorieID: Int): Collection<Lokalisierung>
-	suspend fun getForKartentext(kartentextID: Int): Collection<Lokalisierung>
+	suspend fun getForSpiel(spielID: SpielelementID.SpielID): Collection<Lokalisierung>
+	suspend fun getForKategorie(kategorieID: SpielelementID.KategorieID): Collection<Lokalisierung>
+	suspend fun getForKartentext(kartentextID: SpielelementID.KartentextID): Collection<Lokalisierung>
 }
