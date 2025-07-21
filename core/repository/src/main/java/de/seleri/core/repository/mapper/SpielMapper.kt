258a60360fad88798fd9594dda8d622952ce2a90
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-21 22:00:42 +0200

    SpielEntity.toMeta

--- core/repository/src/main/java/de/seleri/core/repository/mapper/SpielMapper.kt
+++ core/repository/src/main/java/de/seleri/core/repository/mapper/SpielMapper.kt
@@ -3,7 +3,6 @@ package de.seleri.core.repository.mapper
 import de.seleri.core.common.idTypes.SpielelementID
 import de.seleri.core.data.entities.singles.LokalisierungEntity
 import de.seleri.core.data.entities.singles.SpielEntity
-import de.seleri.core.data.entities.singles.SpielelementBasis
 import de.seleri.core.domain.model.DatenbankObjektDaten
 import de.seleri.core.domain.model.Lokalisierung
 import de.seleri.core.domain.model.spielelemente.Kategorie
@@ -11,37 +10,53 @@ import de.seleri.core.domain.model.spielelemente.SpielelementDaten
 import de.seleri.core.domain.model.spielelemente.sammlungen.SammlungDaten
 import de.seleri.core.domain.model.spielelemente.spiel.Spiel
 import de.seleri.core.domain.model.spielelemente.spiel.SpielMetaDaten
+import de.seleri.core.domain.model.spielelemente.spiel.SpielMetaObjekt
 
 object SpielMapper: SpielelementMapper<SpielelementID.SpielID> {
 
 	override fun lokalisierungToEntity(
 		id: SpielelementID.SpielID, lokalisierung: Lokalisierung
-	): LokalisierungEntity {
-		return lokalisierung.toEntityForSpiel(id)
-	}
+	): LokalisierungEntity =
+		lokalisierung.toEntityForSpiel(id)
 }
 
-fun Spiel.toEntity(): SpielEntity {
-	return SpielEntity(
-		id = id, spielelementBasis = SpielelementBasis(
-			selbstErstellt = selbstErstellt, inaktiv = inaktiv, favorisiert = favorisiert, ogSprache = ogSprache
-		), anleitung = anleitung, texteProKarte = texteProKarte, bildDateiname = bildDateiname
+
+fun Spiel.toEntity(): SpielEntity =
+	SpielEntity(
+		id = id,
+		spielelementBasis = domainToSpielelmentBasis(this),
+		anleitung = anleitung,
+		texteProKarte = texteProKarte,
+		bildDateiname = bildDateiname
 	)
-}
+
 
 fun SpielEntity.toDomain(
 	lokalisierungen: Collection<Lokalisierung>, kategorien: Collection<Kategorie>
-): Spiel {
-	return Spiel(
-		spielMetaDaten = SpielMetaDaten(
-			spielelementDaten = SpielelementDaten(
-				DatenbankObjektDaten(id = id),
-				selbstErstellt = spielelementBasis.selbstErstellt,
-				inaktiv = spielelementBasis.inaktiv,
-				favorisiert = spielelementBasis.favorisiert,
-				ogSprache = spielelementBasis.ogSprache,
-				lokalisierungen = lokalisierungen
-			), bildDateiname = bildDateiname
-		), anleitung = anleitung, texteProKarte = texteProKarte, sammlungDaten = SammlungDaten(kategorien)
+): Spiel =
+	Spiel(
+		spielMetaDaten = entityToSpielMetaDaten(this, lokalisierungen),
+		anleitung = anleitung,
+		texteProKarte = texteProKarte,
+		sammlungDaten = SammlungDaten(kategorien)
 	)
-}
+
+
+fun SpielEntity.toMeta(
+	lokalisierungen: Collection<Lokalisierung>
+): SpielMetaObjekt =
+	SpielMetaObjekt(
+		spielMetaDaten = entityToSpielMetaDaten(this, lokalisierungen)
+	)
+
+private fun entityToSpielMetaDaten(entity: SpielEntity, lokalisierungen: Collection<Lokalisierung>): SpielMetaDaten =
+	SpielMetaDaten(
+		spielelementDaten = SpielelementDaten(
+			DatenbankObjektDaten(id = entity.id),
+			selbstErstellt = entity.spielelementBasis.selbstErstellt,
+			inaktiv = entity.spielelementBasis.inaktiv,
+			favorisiert = entity.spielelementBasis.favorisiert,
+			ogSprache = entity.spielelementBasis.ogSprache,
+			lokalisierungen = lokalisierungen
+		), bildDateiname = entity.bildDateiname
+	)
