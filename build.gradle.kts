62d5fbb071f1a00a487c70ceb371880221952ddd
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-23 14:02:30 +0200

    Project-Gradle kommentiert

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-	alias(libs.plugins.androidLibrary) apply false
-  alias(libs.plugins.androidApplication) apply false
-  alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-	alias(libs.plugins.jetbrainsKotlinJvm) apply false
-  alias(libs.plugins.detekt) apply false
-  alias(libs.plugins.ksp) apply false
-  alias(libs.plugins.hilt.android) apply false
-  alias(libs.plugins.room) apply false
+	alias(libs.plugins.jetbrainsKotlinJvm) apply false // für ein Kotlin-Modul (nicht mit Android kombinierbar)
+	alias(libs.plugins.jetbrainsKotlinAndroid) apply false // für ein Android-Modul (nicht mit Kotlin kombinierbar)
+
+	alias(libs.plugins.androidApplication) apply false // für ein Android-App-Modul
+	alias(libs.plugins.androidLibrary) apply false // für ein Android-Lib-Modul
+
+	alias(libs.plugins.detekt) apply false // Detekt
+
+	alias(libs.plugins.ksp) apply false // KSP; benötigt für Hilt, Room
+	alias(libs.plugins.hilt.android) apply false // Hilt
+	alias(libs.plugins.room) apply false // Room
 }
