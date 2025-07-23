254294e0c978a5c16a83ad819badfd21a7c8b630
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-24 01:40:06 +0200

    alles ist möglich!

--- core/representation/src/main/java/de/seleri/core/representation/viewModels/SpielelementAlsTextDarstellen.kt
+++ core/representation/src/main/java/de/seleri/core/representation/viewModels/SpielelementAlsTextDarstellen.kt
@@ -8,6 +8,7 @@ interface SpielelementAlsTextDarstellen {
 	val sprache: Sprache
 		get() = Sprache.OG // TODO Sprache muss variabel gesetzt werden
 
-	fun getDarstellungAlsText(spielelement: Spielelement): String =
-		spielelement.getBezeichnung(sprache)
+	fun getDarstellungAlsText(spielelement: Spielelement?): String =
+		spielelement?.getBezeichnung(sprache)
+			?: "loading"
 }
