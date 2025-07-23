1e6681d8ce9a5141501fce97d8e70d376454c3ea
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-24 01:49:59 +0200

    Spielscreen angefangen

---
+++ core/representation/src/main/java/de/seleri/core/representation/ui/screens/Spielscreen.kt
@@ -0,0 +1,122 @@
+package de.seleri.core.representation.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import de.seleri.core.representation.viewModels.SpielscreenVM
+
+@Composable
+fun Spielscreen(
+	vm: SpielscreenVM
+) {
+	val spiel = vm.spiel
+	Column(
+		modifier = Modifier
+			.fillMaxSize()
+			.background(MaterialTheme.colorScheme.background)
+	) {
+		Spacer(modifier = Modifier.height(10.dp))
+
+		SpielScreenRow {
+			SpielTitel(vm.getDarstellungAlsText(spiel), Modifier.weight(1f))
+			SpielScreenIcon(SpielScreenIcons.Einstellungsrad)
+		}
+
+		val kartentexte = remember { mutableStateMapOf<Int, String>() }
+		LazyColumn(
+			modifier = Modifier
+				.weight(1f)
+				.fillMaxWidth(), verticalArrangement = Arrangement.Center
+		) {
+			items(
+				spiel
+					.getAktuelleKategorien()
+					.toList(), key = { it.id }) { kategorie ->
+				val initialText = viewModel.getName(kategorie)
+				val kartentext = kartentexte[kategorie.id]
+					?: initialText
+
+				SammlungsButton(kartentext) {
+					kartentexte[kategorie.id] = viewModel.getRandomKartentext(kategorie)
+				}
+			}
+		}
+
+		SpielScreenRow {
+			SpielScreenIcon(SpielScreenIcons.PfeilFuerLetzteKarte)
+			SpielScreenIcon(SpielScreenIcons.KarteLoeschen)
+		}
+	}
+}
+
+/**
+ * Gruppierung von den Row-Inhalten des [SpielScreen]
+ *
+ * @param content Inhalt der Row
+ */
+@Composable
+fun SpielScreenRow(content: @Composable RowScope.() -> Unit) {
+	Row(
+		modifier = Modifier
+			.fillMaxWidth()
+			.padding(horizontal = 16.dp, vertical = 0.dp),
+		horizontalArrangement = Arrangement.SpaceBetween,
+		verticalAlignment = Alignment.CenterVertically
+	) {
+		content()
+	}
+}
+
+/**
+ * Darstellung des Spiele-Titels
+ *
+ * @param name Name des Spiels aus [Spiel.localizations]
+ * @param modifier [Modifier] für die Darstellung in [SpielScreenRow]
+ */
+@Composable
+fun SpielTitel(name: String, modifier: Modifier) {
+	Text(
+		text = name,
+		color = MaterialTheme.colorScheme.primary,
+		style = MaterialTheme.typography.displayMedium,
+		maxLines = 2,
+		overflow = TextOverflow.Ellipsis,
+		modifier = modifier
+	)
+}
+
+/**
+ * Darstellung eines Icons für den [SpielScreen]
+ *
+ * @param icon Icon aus [SpielScreenIcons]
+ */
+@Composable
+fun SpielScreenIcon(icon: SpielScreenIcons) {
+	Icon(
+		painter = painterResource(icon.ressource),
+		contentDescription = stringResource(icon.beschreibung),
+		tint = MaterialTheme.colorScheme.primaryContainer,
+		modifier = Modifier.size(50.dp)
+	)
+}
