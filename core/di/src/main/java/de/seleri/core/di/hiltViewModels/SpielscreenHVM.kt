4b6ed8580d7e4faa50b81a36df92ccaa2e2445eb
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-24 00:58:58 +0200

    SpielscreenVM samt Hilt-Version erstellt

--- core/di/src/main/java/de/seleri/core/di/hiltViewModels/SpielscreenHVM.kt
+++ core/di/src/main/java/de/seleri/core/di/hiltViewModels/SpielscreenHVM.kt
@@ -1,5 +1,12 @@
 package de.seleri.core.di.hiltViewModels
 
-class SpielscreenHVM {
-	// TODO
-}
+import dagger.hilt.android.lifecycle.HiltViewModel
+import de.seleri.core.domain.model.idTypes.SpielelementID
+import de.seleri.core.domain.useCases.GetSpielUC
+import de.seleri.core.representation.viewModels.SpielscreenVM
+import javax.inject.Inject
+
+@HiltViewModel
+class SpielscreenHVM @Inject constructor(
+	spielID: SpielelementID.SpielID, getSpielUC: GetSpielUC
+): SpielscreenVM(spielID, getSpielUC)
--- core/representation/src/main/java/de/seleri/core/representation/viewModels/SpielscreenVM.kt
+++ core/representation/src/main/java/de/seleri/core/representation/viewModels/SpielscreenVM.kt
@@ -1,7 +1,25 @@
 package de.seleri.core.representation.viewModels
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import de.seleri.core.domain.model.idTypes.SpielelementID
+import de.seleri.core.domain.model.spielelemente.spiel.Spiel
+import de.seleri.core.domain.useCases.GetSpielUC
+import kotlinx.coroutines.launch
 
-class SpielscreenVM: ViewModel() {
-	// TODO
+open class SpielscreenVM(
+	private val spielID: SpielelementID.SpielID, private val getSpielUC: GetSpielUC
+): ViewModel(), SpielelementAlsTextDarstellen {
+
+	var spiel by mutableStateOf<Spiel?>(null)
+		private set
+
+	init {
+		viewModelScope.launch {
+			spiel = getSpielUC(spielID)
+		}
+	}
 }
