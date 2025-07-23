17b5e65b6b16834d1f4f3a306c773894b51b8f44
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-23 02:01:42 +0200

    gradle eingedampft

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,86 +1,19 @@
-import kotlinx.kover.gradle.plugin.dsl.CoverageUnit
-
 plugins {
-	alias(libs.plugins.androidLibrary)
-	alias(libs.plugins.jetbrainsKotlinAndroid)
+	kotlin("jvm")
 
 	alias(libs.plugins.detekt)
-	alias(libs.plugins.kover)
-}
-
-android {
-	namespace = "de.seleri.core.common"
-	compileSdk = project
-		.property("compileSdk")
-		.toString()
-		.toInt()
-
-	buildTypes {
-		release {
-			isMinifyEnabled = false
-			proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-		}
-	}
-
-	compileOptions {
-		sourceCompatibility = JavaVersion.VERSION_11
-		targetCompatibility = JavaVersion.VERSION_11
-	}
-
-	lint {
-		warningsAsErrors = true
-	}
 }
 
 kotlin {
-	compilerOptions {
-		jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+	jvmToolchain(21)
+}
+
+java {
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(21))
 	}
 }
 
 dependencies {
-
-	implementation(libs.androidx.core.ktx)
-	implementation(libs.androidx.appcompat)
-	implementation(libs.material)
 	testImplementation(libs.junit)
-	androidTestImplementation(libs.androidx.junit)
-	androidTestImplementation(libs.androidx.espresso.core)
-}
-
-kover {
-	reports {
-		filters {
-			excludes {
-				classes(
-					"",
-				)
-				packages(
-					"",
-				)
-			}
-		}
-		verify {
-			warningInsteadOfFailure = false
-
-			rule("genug Abzweigungen getestet") {
-				bound {
-					minValue = 66
-					coverageUnits = CoverageUnit.BRANCH
-				}
-			}
-			rule("genug Anweisungen getestet") {
-				bound {
-					minValue = 66
-					coverageUnits = CoverageUnit.INSTRUCTION
-				}
-			}
-			rule("genug Zeilen getestet") {
-				bound {
-					minValue = 66
-					coverageUnits = CoverageUnit.LINE
-				}
-			}
-		}
-	}
 }
