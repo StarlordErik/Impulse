c78c4761ebc6295edec5903a763e910be8f00398
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-24 00:58:19 +0200

    Gemeinsamkeiten ausgelagert

---
+++ core/representation/src/main/java/de/seleri/core/representation/viewModels/SpielelementAlsTextDarstellen.kt
@@ -0,0 +1,13 @@
+package de.seleri.core.representation.viewModels
+
+import de.seleri.core.common.Sprache
+import de.seleri.core.domain.model.spielelemente.Spielelement
+
+interface SpielelementAlsTextDarstellen {
+
+	val sprache: Sprache
+		get() = Sprache.OG // TODO Sprache muss variabel gesetzt werden
+
+	fun getDarstellungAlsText(spielelement: Spielelement): String =
+		spielelement.getBezeichnung(sprache)
+}
--- core/representation/src/main/java/de/seleri/core/representation/viewModels/StartscreenVM.kt
+++ core/representation/src/main/java/de/seleri/core/representation/viewModels/StartscreenVM.kt
@@ -5,30 +5,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import de.seleri.core.common.Sprache
 import de.seleri.core.domain.model.spielelemente.spiel.SpielMetaObjekt
 import de.seleri.core.domain.useCases.GetAllSpielMetaObjekteUC
 import kotlinx.coroutines.launch
 
 open class StartscreenVM(
 	private val getAllSpielMetasUseCase: GetAllSpielMetaObjekteUC
-): ViewModel() {
+): ViewModel(), SpielelementAlsTextDarstellen {
 
 	var spielMetaObjekte by mutableStateOf<List<SpielMetaObjekt>>(emptyList())
 		private set
 
 	init {
-		loadSpielMetaObjekte()
-	}
-
-	private fun loadSpielMetaObjekte() {
 		viewModelScope.launch {
 			spielMetaObjekte = getAllSpielMetasUseCase()
 		}
 	}
-
-	val sprache = Sprache.OG // TODO Sprache muss variabel gesetzt werden
-
-	fun getDarstellungAlsText(spielMetaObjekt: SpielMetaObjekt): String =
-		spielMetaObjekt.getBezeichnung(sprache)
 }
