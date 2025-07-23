2cccbfb355ded1b4d15a21e5e4da50680cd0afce
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-24 01:03:40 +0200

    GetSpielUC fertig gestellt

--- core/di/src/main/java/de/seleri/core/di/module/UsecaseModule.kt
+++ core/di/src/main/java/de/seleri/core/di/module/UsecaseModule.kt
@@ -6,12 +6,17 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import de.seleri.core.domain.repositories.SpielRepo
 import de.seleri.core.domain.useCases.GetAllSpielMetaObjekteUC
+import de.seleri.core.domain.useCases.GetSpielUC
 
 @Module
 @InstallIn(SingletonComponent::class)
 object UsecaseModule {
 
 	@Provides
-	fun provideGetAlleSpielMetasUsecase(spielRepo: SpielRepo): GetAllSpielMetaObjekteUC =
+	fun provideGetAlleSpielMetasUC(spielRepo: SpielRepo): GetAllSpielMetaObjekteUC =
 		GetAllSpielMetaObjekteUC(spielRepo)
+
+	@Provides
+	fun provideGetSpielUC(spielRepo: SpielRepo): GetSpielUC =
+		GetSpielUC(spielRepo)
 }
--- core/domain/src/main/java/de/seleri/core/domain/useCases/GetSpielUC.kt
+++ core/domain/src/main/java/de/seleri/core/domain/useCases/GetSpielUC.kt
@@ -1,13 +1,11 @@
 package de.seleri.core.domain.useCases
 
+import de.seleri.core.domain.model.idTypes.SpielelementID
 import de.seleri.core.domain.model.spielelemente.spiel.Spiel
-import de.seleri.core.domain.repositories.LokalisierungRepo
+import de.seleri.core.domain.repositories.SpielRepo
 
-class GetSpielUC(private val lokalisierungRepo: LokalisierungRepo) {
+class GetSpielUC(private val spielRepo: SpielRepo) {
 
-	suspend operator fun invoke(spielID: Int): Spiel {
-		// val lokalisierungen = lokalisierungRepo.getForSpiel(spielID)
-		TODO()
-		// return spielEntity.toDomain(lokalisierungen)
-	}
+	suspend operator fun invoke(spielID: SpielelementID.SpielID): Spiel =
+		spielRepo.get(spielID)
 }
