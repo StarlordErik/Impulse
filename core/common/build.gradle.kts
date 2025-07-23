b2a31f0dc17c82df65a4fab893dd696d91309904
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-23 02:23:20 +0200

    Das Kotlin-Standard-Modul geschaffen!

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,7 +1,51 @@
+import kotlinx.kover.gradle.plugin.dsl.CoverageUnit
+
 plugins {
 	kotlin("jvm")
 
 	alias(libs.plugins.detekt)
+	alias(libs.plugins.kover)
+}
+
+dependencies {
+	testImplementation(libs.junit)
+}
+
+kover {
+	reports {
+		filters {
+			excludes {
+				classes(
+					"",
+				)
+				packages(
+					"",
+				)
+			}
+		}
+		verify {
+			warningInsteadOfFailure = false
+
+			val min = project
+				.property("koverMinValue")
+				.toString()
+				.toInt()
+			rule("Mindestens $min% getestet!") {
+				bound {
+					minValue = min
+					coverageUnits = CoverageUnit.BRANCH
+				}
+				bound {
+					minValue = min
+					coverageUnits = CoverageUnit.INSTRUCTION
+				}
+				bound {
+					minValue = min
+					coverageUnits = CoverageUnit.LINE
+				}
+			}
+		}
+	}
 }
 
 kotlin {
@@ -24,8 +68,4 @@ java {
 			)
 		)
 	}
-}
-
-dependencies {
-	testImplementation(libs.junit)
 }
