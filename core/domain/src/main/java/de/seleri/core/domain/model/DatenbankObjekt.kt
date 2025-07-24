be5a828ef943130c8e9e6bf825158601e2b8ea5d
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-24 17:23:07 +0200

    Comparable vorerst nicht benötigt

--- core/domain/src/main/java/de/seleri/core/domain/model/DatenbankObjekt.kt
+++ core/domain/src/main/java/de/seleri/core/domain/model/DatenbankObjekt.kt
@@ -1,10 +1,6 @@
 package de.seleri.core.domain.model
 
-interface DatenbankObjekt: Comparable<DatenbankObjekt> {
+interface DatenbankObjekt {
 
 	val id: Int
-
-	// TODO muss auch über Entity-Namen gehen! (SpielMetaObjekt == Spiel)
-	override fun compareTo(other: DatenbankObjekt): Int =
-		this.id.compareTo(other.id)
 }
