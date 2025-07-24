46e3a0a151f7e64e13694f74f4cc6be0ccc0864d
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-24 16:41:27 +0200

    Datenbankobjekte sind jetzt nur über die ID verschieden

--- core/domain/src/main/java/de/seleri/core/domain/model/DatenbankObjekt.kt
+++ core/domain/src/main/java/de/seleri/core/domain/model/DatenbankObjekt.kt
@@ -1,6 +1,10 @@
 package de.seleri.core.domain.model
 
-interface DatenbankObjekt {
+interface DatenbankObjekt: Comparable<DatenbankObjekt> {
 
 	val id: Int
+
+	// TODO muss auch über Entity-Namen gehen! (SpielMetaObjekt == Spiel)
+	override fun compareTo(other: DatenbankObjekt): Int =
+		this.id.compareTo(other.id)
 }
