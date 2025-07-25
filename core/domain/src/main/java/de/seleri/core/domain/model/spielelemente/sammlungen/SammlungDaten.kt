826e57209bc4d48bcac279a10753dcdc85c25f6f
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-25 17:26:47 +0200

    SammlungDaten funktioniert, ist aber extrem nutzlos

--- core/domain/src/main/java/de/seleri/core/domain/model/spielelemente/sammlungen/SammlungDaten.kt
+++ core/domain/src/main/java/de/seleri/core/domain/model/spielelemente/sammlungen/SammlungDaten.kt
@@ -1,5 +1,24 @@
 package de.seleri.core.domain.model.spielelemente.sammlungen
 
+import de.seleri.core.domain.model.spielelemente.Kartentext
+import de.seleri.core.domain.model.spielelemente.Kategorie
+
 data class SammlungDaten<B: Bestandteil>(
 	override val bestandteile: Collection<B>,
-): Sammlung<B>
+): Sammlung<B> {
+
+	/**
+	 * Das Problem an der Funktion ist wie folgt: Sammlung definiert diese Funktion - und alle Sammlungen wie Spiel
+	 * und Kategorie haben auch eine sinnvolle Weise, diese Funktion zu implementieren. Aber das geht nur, weil sie
+	 * wissen, was ihr konkreter Bestandteil ist, auf den sie dann weitere Funktionen aufrufen können. Da die Daten
+	 * zu Sammlung, also [SammlungDaten], aber auch eine Sammlung sind... ist das ganze etwas lost.
+	 *
+	 * MÖGLICHE LÖSUNG: Wenn SammlungDaten nur Daten hält, aber keine Funktionen ausführt, kann man die Delegation
+	 * auch lassen.
+	 */
+	@Deprecated("Muss von verwendeten Klassen überschrieben werden.")
+	override fun getKategorieMitKarte(
+		anzahlTexte: Int, bereitsEnthalteneKT: Collection<Kartentext>
+	): Pair<Kategorie, List<Kartentext>> =
+		error("Muss von Implementierungen überschrieben werden.")
+}
