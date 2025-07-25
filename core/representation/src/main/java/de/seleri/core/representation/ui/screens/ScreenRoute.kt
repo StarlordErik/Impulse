ed00369182386f11c7f958a8ecabe23aaad706a1
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-25 15:27:08 +0200

    ScreenRoute nun mit SpielelementID-Typ-Sicherheit

--- core/representation/src/main/java/de/seleri/core/representation/ui/screens/ScreenRoute.kt
+++ core/representation/src/main/java/de/seleri/core/representation/ui/screens/ScreenRoute.kt
@@ -1,9 +1,11 @@
 package de.seleri.core.representation.ui.screens
 
+import de.seleri.core.common.idTypes.SpielelementID
+
 sealed class ScreenRoute(val route: String) { object StartscreenRoute: ScreenRoute("start")
 	object SpielscreenRoute: ScreenRoute("spiel") {
 
-		fun mitDerSpielID(spielID: Int) =
-			"$route/${spielID.toInt()}" // TODO Spielelement.ID
+		fun mitDerSpielID(spielID: SpielelementID.SpielID) =
+			"$route/${spielID.toInt()}"
 	}
 }
