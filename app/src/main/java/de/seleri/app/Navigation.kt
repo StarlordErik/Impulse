cbdcd754f95a1bd442556db075822a4adc941562
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-25 16:26:11 +0200

    Spielscreen-Navigation vereinfacht

--- app/src/main/java/de/seleri/app/Navigation.kt
+++ app/src/main/java/de/seleri/app/Navigation.kt
@@ -3,17 +3,13 @@ package de.seleri.app
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import dagger.hilt.android.EntryPointAccessors
-import de.seleri.core.common.idTypes.SpielelementID
-import de.seleri.core.di.hiltViewModels.SpielscreenHVMFactoryProvider
+import de.seleri.core.di.hiltViewModels.SpielscreenHVM
 import de.seleri.core.di.hiltViewModels.StartscreenHVM
 import de.seleri.core.representation.ui.screens.ScreenRoute
 import de.seleri.core.representation.ui.screens.Spielscreen
@@ -72,17 +68,8 @@ fun Navigation() {
 				navArgument("spielID") {
 					type = NavType.IntType
 					nullable = false
-				})
-		) { eingabe ->
-			val spielID = SpielelementID.SpielID(eingabe.arguments!!.getInt("spielID")) // TODO UNUSED
-
-			val factory = EntryPointAccessors
-				.fromApplication(
-					LocalContext.current.applicationContext, SpielscreenHVMFactoryProvider::class.java
-				)
-				.spielscreenHVMFactory()
-			val spielscreenHVM = remember { factory.create(spielID) }
-
+				})) {
+			val spielscreenHVM: SpielscreenHVM = hiltViewModel()
 			Spielscreen(vm = spielscreenHVM)
 		}
 	}
--- core/di/src/main/java/de/seleri/core/di/hiltViewModels/SpielscreenHVM.kt
+++ core/di/src/main/java/de/seleri/core/di/hiltViewModels/SpielscreenHVM.kt
@@ -1,35 +1,18 @@
 package de.seleri.core.di.hiltViewModels
 
-import dagger.assisted.Assisted
-import dagger.assisted.AssistedFactory
-import dagger.assisted.AssistedInject
-import dagger.hilt.EntryPoint
-import dagger.hilt.InstallIn
+import androidx.lifecycle.SavedStateHandle
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.components.SingletonComponent
 import de.seleri.core.common.idTypes.SpielelementID
 import de.seleri.core.domain.useCases.GetKategorieMitKarteUC
 import de.seleri.core.domain.useCases.GetSpielUC
 import de.seleri.core.representation.viewModels.SpielscreenVM
+import javax.inject.Inject
 
 @HiltViewModel
-class SpielscreenHVM @AssistedInject constructor(
-	@Assisted
-	spielID: SpielelementID.SpielID,
-
-	getSpielUC: GetSpielUC, getKategorieMitKarteUC: GetKategorieMitKarteUC
-): SpielscreenVM(spielID, getSpielUC, getKategorieMitKarteUC) {
-
-	@AssistedFactory
-	interface Factory {
-
-		fun create(spielID: SpielelementID.SpielID): SpielscreenHVM
-	}
-}
-
-@EntryPoint
-@InstallIn(SingletonComponent::class)
-interface SpielscreenHVMFactoryProvider {
-
-	fun spielscreenHVMFactory(): SpielscreenHVM.Factory
-}
+class SpielscreenHVM @Inject constructor(
+	savedStateHandle: SavedStateHandle, getSpielUC: GetSpielUC, getKategorieMitKarteUC: GetKategorieMitKarteUC
+): SpielscreenVM(
+	spielID = SpielelementID.SpielID(savedStateHandle.get<Int>("spielID")!!),
+	getSpielUC = getSpielUC,
+	getKategorieMitKarteUC = getKategorieMitKarteUC
+)
