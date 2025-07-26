90f56390660ab758477e4ad4d1144e498e454d29
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-26 13:28:57 +0200

    SpielelementDaten.fromEingabe() erstellt

--- core/domain/src/main/java/de/seleri/core/domain/model/spielelemente/SpielelementDaten.kt
+++ core/domain/src/main/java/de/seleri/core/domain/model/spielelemente/SpielelementDaten.kt
@@ -3,6 +3,7 @@ package de.seleri.core.domain.model.spielelemente
 import de.seleri.core.common.Sprache
 import de.seleri.core.domain.model.DatenbankObjekt
 import de.seleri.core.domain.model.DatenbankObjektDaten
+import de.seleri.core.domain.model.Konstanten
 import de.seleri.core.domain.model.Lokalisierung
 
 data class SpielelementDaten(
@@ -10,8 +11,29 @@ data class SpielelementDaten(
 
 	override val lokalisierungen: Collection<Lokalisierung>,
 
-	override val ogSprache: Sprache = Sprache.DE,
-	override val selbstErstellt: Boolean = false,
-	override val inaktiv: Boolean = false,
-	override val favorisiert: Boolean = false,
-): DatenbankObjekt by datenbankObjektDaten, Spielelement
+	override val ogSprache: Sprache = Konstanten.OG_SPRACHE,
+	override val selbstErstellt: Boolean = Konstanten.SELBST_ERSTELLT,
+	override val inaktiv: Boolean = Konstanten.INAKTIV,
+	override val favorisiert: Boolean = Konstanten.FAVORISIERT,
+): DatenbankObjekt by datenbankObjektDaten, Spielelement {
+
+	companion object {
+
+		fun fromEingabe(
+			id: Int = Konstanten.ID,
+			lokalisierungen: Collection<Lokalisierung>,
+			ogSprache: Sprache = Konstanten.OG_SPRACHE,
+			selbstErstellt: Boolean = Konstanten.SELBST_ERSTELLT,
+			inaktiv: Boolean = Konstanten.INAKTIV,
+			favorisiert: Boolean = Konstanten.FAVORISIERT
+		): SpielelementDaten =
+			SpielelementDaten(
+				datenbankObjektDaten = DatenbankObjektDaten.fromEingabe(id),
+				lokalisierungen = lokalisierungen,
+				ogSprache = ogSprache,
+				selbstErstellt = selbstErstellt,
+				inaktiv = inaktiv,
+				favorisiert = favorisiert
+			)
+	}
+}
