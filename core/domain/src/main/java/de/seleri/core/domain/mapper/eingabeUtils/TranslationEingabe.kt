4839fde23a78a09685894c0ee8bb5fe8993bcfed
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-27 23:47:16 +0200

    Translation-Generator durch Eingabe in die Eingabe-Utils verschoben

--- core/domain/src/main/java/de/seleri/core/domain/mapper/eingabeUtils/TranslationEingabe.kt
+++ core/domain/src/main/java/de/seleri/core/domain/mapper/eingabeUtils/TranslationEingabe.kt
@@ -2,9 +2,19 @@ package de.seleri.core.domain.mapper.eingabeUtils
 
 import de.seleri.core.common.Sprache
 import de.seleri.core.common.idInt.TranslationIDint
+import de.seleri.core.domain.modell.Translation
 
 const val TRANSLATION_ID_BOOSTER = 1000000
 
 data class TranslationEingabe(
 	val id: TranslationIDint, val sprache: Sprache = Sprache.OG, val bezeichnung: String
 )
+
+fun generiereTranslation(
+	translationEingabe: TranslationEingabe
+): Translation =
+	Translation(
+		id = translationEingabe.id,
+		sprache = translationEingabe.sprache,
+		bezeichnung = translationEingabe.bezeichnung,
+	)
--- core/domain/src/main/java/de/seleri/core/domain/modell/Translation.kt
+++ core/domain/src/main/java/de/seleri/core/domain/modell/Translation.kt
@@ -2,7 +2,6 @@ package de.seleri.core.domain.modell
 
 import de.seleri.core.common.Sprache
 import de.seleri.core.common.idInt.TranslationIDint
-import de.seleri.core.domain.mapper.eingabeUtils.TranslationEingabe
 
 data class Translation(
 	override val id: TranslationIDint,
@@ -10,17 +9,4 @@ data class Translation(
 	val sprache: Sprache,
 	val bezeichnung: String,
 	val bearbeitet: Boolean = Konstanten.BEARBEITET,
-): EntityModell {
-
-	companion object {
-
-		fun forInitialdaten(
-			translationEingabe: TranslationEingabe
-		): Translation =
-			Translation(
-				id = translationEingabe.id,
-				sprache = translationEingabe.sprache,
-				bezeichnung = translationEingabe.bezeichnung,
-			)
-	}
-}
+): EntityModell
