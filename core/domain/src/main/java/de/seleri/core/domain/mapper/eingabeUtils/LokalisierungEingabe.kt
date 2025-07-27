0ab11d1b30cc99f8a4e8c0407dcdc37f72924823
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-27 23:49:09 +0200

    Lokalisierung-Generator durch Eingabe in die Eingabe-Utils verschoben

--- core/domain/src/main/java/de/seleri/core/domain/mapper/eingabeUtils/LokalisierungEingabe.kt
+++ core/domain/src/main/java/de/seleri/core/domain/mapper/eingabeUtils/LokalisierungEingabe.kt
@@ -1,7 +1,31 @@
 package de.seleri.core.domain.mapper.eingabeUtils
 
+import de.seleri.core.common.Sprache
 import de.seleri.core.common.idInt.LokalisierungIDint
+import de.seleri.core.common.idInt.TranslationIDint
+import de.seleri.core.domain.modell.Lokalisierung
 
 data class LokalisierungEingabe(
 	val id: LokalisierungIDint, val translationen: Collection<TranslationEingabe>
 )
+
+fun generiereLokalisierung(
+	ogSprache: Sprache, lokalisierungEingabe: LokalisierungEingabe
+): Lokalisierung {
+	val mehrTranslationen = lokalisierungEingabe.translationen.toMutableList()
+
+	val vorhandeneSprachen = lokalisierungEingabe.translationen.map { it.sprache }
+	val ogTranslation = lokalisierungEingabe.translationen.find { it.sprache == ogSprache }
+
+	if (ogSprache !in vorhandeneSprachen && ogTranslation != null) {
+		val neueID = ogTranslation.id.translationID + TRANSLATION_ID_BOOSTER
+		mehrTranslationen += TranslationEingabe(
+			id = TranslationIDint(neueID), sprache = ogSprache, bezeichnung = ogTranslation.bezeichnung
+		)
+	}
+
+	return Lokalisierung(
+		id = lokalisierungEingabe.id,
+		ogSprache = ogSprache,
+		translationen = mehrTranslationen.map { generiereTranslation(it) })
+}
--- core/domain/src/main/java/de/seleri/core/domain/modell/Lokalisierung.kt
+++ core/domain/src/main/java/de/seleri/core/domain/modell/Lokalisierung.kt
@@ -2,38 +2,9 @@ package de.seleri.core.domain.modell
 
 import de.seleri.core.common.Sprache
 import de.seleri.core.common.idInt.LokalisierungIDint
-import de.seleri.core.common.idInt.TranslationIDint
-import de.seleri.core.domain.mapper.eingabeUtils.LokalisierungEingabe
-import de.seleri.core.domain.mapper.eingabeUtils.TRANSLATION_ID_BOOSTER
-import de.seleri.core.domain.mapper.eingabeUtils.TranslationEingabe
 
 data class Lokalisierung(
 	override val id: LokalisierungIDint,
 
 	val ogSprache: Sprache, val translationen: Collection<Translation>
-): EntityModell {
-
-	companion object {
-
-		fun forInitialdaten(
-			ogSprache: Sprache, lokalisierungEingabe: LokalisierungEingabe
-		): Lokalisierung {
-			val mehrTranslationen = lokalisierungEingabe.translationen.toMutableList()
-
-			val vorhandeneSprachen = lokalisierungEingabe.translationen.map { it.sprache }
-			val ogTranslation = lokalisierungEingabe.translationen.find { it.sprache == ogSprache }
-
-			if (ogSprache !in vorhandeneSprachen && ogTranslation != null) {
-				val neueID = ogTranslation.id.translationID + TRANSLATION_ID_BOOSTER
-				mehrTranslationen += TranslationEingabe(
-					id = TranslationIDint(neueID), sprache = ogSprache, bezeichnung = ogTranslation.bezeichnung
-				)
-			}
-
-			return Lokalisierung(
-				id = lokalisierungEingabe.id,
-				ogSprache = ogSprache,
-				translationen = mehrTranslationen.map { Translation.forInitialdaten(it) })
-		}
-	}
-}
+): EntityModell
