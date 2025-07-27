2efd9d582a30a21d4d02e4402abb0c933ba0bbd0
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-27 20:27:59 +0200

    Bugfix

--- core/domain/src/main/java/de/seleri/core/domain/mapper/DatenbankSlice.kt
+++ core/domain/src/main/java/de/seleri/core/domain/mapper/DatenbankSlice.kt
@@ -1,19 +1,20 @@
 package de.seleri.core.domain.mapper
 
+import de.seleri.core.domain.entities.joins.KategorieXkartentext
 import de.seleri.core.domain.entities.joins.SpielXkategorie
+import de.seleri.core.domain.entities.singles.LokalisierungEntity
 import de.seleri.core.domain.entities.singles.TranslationEntity
 import de.seleri.core.domain.entities.singles.spielelemente.KartentextEntity
 import de.seleri.core.domain.entities.singles.spielelemente.KategorieEntity
 import de.seleri.core.domain.entities.singles.spielelemente.SpielEntity
-import de.seleri.core.domain.modell.Lokalisierung
 
 data class DatenbankSlice(
 	val translationen: Collection<TranslationEntity>? = null,
-	val lokalisierungen: Collection<Lokalisierung>? = null,
+	val lokalisierungen: Collection<LokalisierungEntity>? = null,
 	val kartentexte: Collection<KartentextEntity>? = null,
 	val kategorien: Collection<KategorieEntity>? = null,
 	val spiele: Collection<SpielEntity>? = null,
 
-	val kategorieXkartentexte: Collection<KategorieEntity>? = null,
+	val kategorieXkartentexte: Collection<KategorieXkartentext>? = null,
 	val spielXkategorien: Collection<SpielXkategorie>? = null,
 )
