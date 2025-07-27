fbf17196e01c16b4344a90ad557e187d7ec6dd9f
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-27 20:10:23 +0200

    TranslationsMapper erstellt

---
+++ core/domain/src/main/java/de/seleri/core/domain/mapper/TranslationMapper.kt
@@ -0,0 +1,27 @@
+package de.seleri.core.domain.mapper
+
+import de.seleri.core.common.idInt.LokalisierungIDint
+import de.seleri.core.common.idInt.TranslationIDint
+import de.seleri.core.domain.entities.singles.TranslationEntity
+import de.seleri.core.domain.modell.EntityModellDaten
+import de.seleri.core.domain.modell.Translation
+
+fun TranslationEntity.toModell(): Translation =
+	Translation(
+		entityModellDaten = EntityModellDaten(id = this.id),
+		sprache = this.sprache,
+		bezeichnung = this.bezeichnung,
+		bearbeitet = this.bearbeitet
+	)
+
+fun Translation.toEntity(lokalisierungsID: LokalisierungIDint): TranslationEntity =
+	TranslationEntity(
+		id = this.id as TranslationIDint,
+		lokalisierungID = lokalisierungsID,
+		sprache = this.sprache,
+		bezeichnung = this.bezeichnung,
+		bearbeitet = this.bearbeitet
+	)
+
+fun Translation.toDatenbankSlice(lokalisierungsID: LokalisierungIDint): DatenbankSlice =
+	DatenbankSlice(translationen = listOf(this.toEntity(lokalisierungsID)))
