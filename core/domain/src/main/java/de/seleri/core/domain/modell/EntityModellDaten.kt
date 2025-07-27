1f5374ace750f742d66cf5ef9cbd3d744d8a9fe3
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-27 17:22:49 +0200

    forInitialdaten!

--- core/domain/src/main/java/de/seleri/core/domain/modell/EntityModellDaten.kt
+++ core/domain/src/main/java/de/seleri/core/domain/modell/EntityModellDaten.kt
@@ -5,11 +5,4 @@ import de.seleri.core.common.idInt.EntityIDint
 
 data class EntityModellDaten(
 	override val id: EntityIDint = Konstanten.ENTITY_ID
-): EntityModell {
-
-	companion object {
-
-		fun fromEingabe(id: EntityIDint = Konstanten.ENTITY_ID): EntityModellDaten =
-			EntityModellDaten(id)
-	}
-}
+): EntityModell
