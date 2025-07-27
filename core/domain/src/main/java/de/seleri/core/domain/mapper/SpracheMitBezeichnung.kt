90cd5f48d2dc10512ed8188af587c3e92dca2f6b
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-27 17:23:22 +0200

    Translation erstellt

---
+++ core/domain/src/main/java/de/seleri/core/domain/mapper/SpracheMitBezeichnung.kt
@@ -0,0 +1,7 @@
+package de.seleri.core.domain.mapper
+
+import de.seleri.core.common.Sprache
+
+data class SpracheMitBezeichnung(
+	val sprache: Sprache, val bezeichnung: String
+)
---
+++ core/domain/src/main/java/de/seleri/core/domain/modell/Translation.kt
@@ -0,0 +1,24 @@
+package de.seleri.core.domain.modell
+
+import de.seleri.core.common.Sprache
+import de.seleri.core.domain.mapper.SpracheMitBezeichnung
+
+data class Translation(
+	private val entityModellDaten: EntityModellDaten = EntityModellDaten(id = Konstanten.TRANSLATION_ID),
+
+	val sprache: Sprache,
+	val bezeichnung: String,
+	val bearbeitet: Boolean = Konstanten.BEARBEITET,
+): EntityModell by entityModellDaten {
+
+	companion object {
+
+		fun forInitialdaten(
+			spracheMitBezeichnung: SpracheMitBezeichnung
+		): Translation =
+			Translation(
+				sprache = spracheMitBezeichnung.sprache,
+				bezeichnung = spracheMitBezeichnung.bezeichnung,
+			)
+	}
+}
