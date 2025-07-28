09b20c44df8c84a7d9362b19778f7c12d01ed6b5
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-28 12:59:08 +0200

    KartentextEingabe-Generator überarbeitet

--- core/domain/src/main/java/de/seleri/core/domain/mapper/eingabeUtils/KartentextEingabe.kt
+++ core/domain/src/main/java/de/seleri/core/domain/mapper/eingabeUtils/KartentextEingabe.kt
@@ -1,7 +1,15 @@
 package de.seleri.core.domain.mapper.eingabeUtils
 
+import de.seleri.core.common.Sprache
 import de.seleri.core.common.idInt.KartentextIDint
+import de.seleri.core.domain.modell.spielelemente.Kartentext
 
 data class KartentextEingabe(
-	val kartentextIDint: KartentextIDint, val translationen: Collection<TranslationEingabe>
+	val kartentextIDint: KartentextIDint, val lokalisierung: LokalisierungEingabe,
 )
+
+fun generiereKartentext(ogSprache: Sprache, kartentextEingabe: KartentextEingabe): Kartentext =
+	Kartentext(
+		id = kartentextEingabe.kartentextIDint,
+		spielelementDaten = generiereSpielelementDaten(ogSprache, kartentextEingabe.lokalisierung),
+	)
