e44e090beb6f0e6f6c9d284fe5fc3c909cc7f63e
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-29 00:41:33 +0200

    Kategorie vom Skript aus erstellbar!

--- core/domain/src/main/java/de/seleri/core/domain/mapper/eingabeUtils/KategorieEingabe.kt
+++ core/domain/src/main/java/de/seleri/core/domain/mapper/eingabeUtils/KategorieEingabe.kt
@@ -1,16 +1,35 @@
 package de.seleri.core.domain.mapper.eingabeUtils
 
-import de.seleri.core.common.Sprache
 import de.seleri.core.common.idInt.KategorieIDint
+import de.seleri.core.domain.modell.Lokalisierung
+import de.seleri.core.domain.modell.spielelemente.Kartentext
 import de.seleri.core.domain.modell.spielelemente.Kategorie
+import de.seleri.core.domain.modell.spielelemente.SpielelementDaten
 
-data class KategorieEingabe(
-	val kategorieIDint: KategorieIDint, val lokalisierungEingabe: LokalisierungEingabe,
-	val kartentexte: Collection<KartentextEingabe>
-)
-
-fun generiereKategorie(ogSprache: Sprache, kategorieEingabe: KategorieEingabe): Kategorie =
-	Kategorie(
-		id = kategorieEingabe.kategorieIDint,
-		spielelementDaten = generiereSpielelementDaten(ogSprache, kategorieEingabe.lokalisierungEingabe),
-		bestandteile = kategorieEingabe.kartentexte.map { generiereKartentext(ogSprache, it) })
+fun Kategorie.Companion.fromSkript(
+	kategorieID: Int, lokalisierung: Lokalisierung?, kartentexte: List<Kartentext>?
+): Kategorie? {
+	if (lokalisierung == null) {
+		if (kartentexte == null) {
+			return null
+		} else {
+			// @formatter:off
+			error("Die Kategorie-Lokalisierung darf nicht null sein, wenn es dazu Kartentexte gibt! Betroffen sind:\n" +
+				"Kartentexte beginnend mit: ${kartentexte.first().lokalisierung.translationen.first()}")
+			// @formatter:on
+		}
+	} else {
+		if (kartentexte == null) {
+			// @formatter:off
+			error("Die Kartentexte dürfen nicht null sein, wenn es dazu eine Kategorie-Lokalisierung gibt! " +
+				"Betroffen ist:\nKategorie: ${lokalisierung.translationen.first()}")
+			// @formatter:on
+		} else {
+			return Kategorie(
+				id = KategorieIDint(kategorieID),
+				spielelementDaten = SpielelementDaten.fromSkript(lokalisierung),
+				bestandteile = kartentexte
+			)
+		}
+	}
+}
