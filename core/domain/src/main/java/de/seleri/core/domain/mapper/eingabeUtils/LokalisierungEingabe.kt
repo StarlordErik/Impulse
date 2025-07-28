4f117c67e191c27daa6bf79c4625833f6d06c601
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-28 23:11:51 +0200

    Lokalisierungen für Kartentexte erstellbar!

--- core/domain/src/main/java/de/seleri/core/domain/mapper/eingabeUtils/LokalisierungEingabe.kt
+++ core/domain/src/main/java/de/seleri/core/domain/mapper/eingabeUtils/LokalisierungEingabe.kt
@@ -8,6 +8,8 @@ import de.seleri.core.domain.modell.Translation
 
 const val DE_ID_BOOSTER = 2
 const val EN_ID_BOOSTER = 3
+const val ANZAHL_TRANSLATION_AUSGABEN_BEI_FEHLER = 3
+const val ANZAHL_TRANSLATION_BUCHSTABEN_PRO_FEHLER = 30
 
 @Suppress("LongParameterList")
 fun Lokalisierung.Companion.fromSkript(
@@ -53,4 +55,55 @@ fun Lokalisierung.Companion.fromSkript(
 			id = LokalisierungIDint(freieLokalisierungID), ogSprache = ogSprache, translationen = translationen
 		)
 	}
+}
+
+@Suppress("LongParameterList")
+fun Lokalisierung.Companion.fromSkriptForKartentexte(
+	freieLokalisierungID: Int,
+	ogSprache: Sprache,
+	ogTranslationen: List<Translation?>,
+	erikTranslationen: List<Translation?>,
+	deTranslationen: List<Translation?>,
+	enTranslationen: List<Translation?>
+): Pair<List<Lokalisierung>, Int> {
+
+	// @formatter:off
+	require(
+		ogTranslationen.size == erikTranslationen.size
+			&& ogTranslationen.size == deTranslationen.size
+			&& ogTranslationen.size == enTranslationen.size
+	) {
+		"Die Kartentext-Translation-Listen sind nicht gleich lang! Betroffen sind die folgenden Kartentexte:\n" +
+			"${ogTranslationen.take(ANZAHL_TRANSLATION_AUSGABEN_BEI_FEHLER).map {
+				it?.bezeichnung?.take(ANZAHL_TRANSLATION_BUCHSTABEN_PRO_FEHLER)
+					?.replace("\n", "\\n")?.replace("\t", "\\t")
+			}}..."
+	}
+	// @formatter:on
+
+	var anzahlNeueLokalisierungen = 0
+
+	val translationenProKartentext = ogTranslationen
+		.zip(erikTranslationen)
+		.zip(deTranslationen.zip(enTranslationen)) { (og, erik), (de, en) ->
+			listOf(og, erik, de, en)
+		}
+
+	val lokalisierungen = mutableListOf<Lokalisierung>()
+
+	translationenProKartentext.forEach { translations ->
+		if (translations.any { it != null }) {
+			val neueID = freieLokalisierungID + anzahlNeueLokalisierungen++
+
+			@Suppress("MagicNumber")
+			val lokalisierung = Lokalisierung.fromSkript(
+				neueID, ogSprache, translations[0], translations[1], translations[2], translations[3]
+			)
+			if (lokalisierung != null) {
+				lokalisierungen += lokalisierung
+			}
+		}
+	}
+
+	return lokalisierungen.toList() to anzahlNeueLokalisierungen
 }
