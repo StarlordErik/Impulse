d9d9dbc10eddf4d4ab3742277fe92b86a6a85d1b
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-28 23:29:03 +0200

    SpielelementDaten und SpielMetaDaten fürs Skript erweitert!

--- core/domain/src/main/java/de/seleri/core/domain/mapper/eingabeUtils/AbstrakeEingabe.kt
+++ core/domain/src/main/java/de/seleri/core/domain/mapper/eingabeUtils/AbstrakeEingabe.kt
@@ -1,11 +1,11 @@
 package de.seleri.core.domain.mapper.eingabeUtils
 
-import de.seleri.core.common.Sprache
+import de.seleri.core.domain.modell.Lokalisierung
 import de.seleri.core.domain.modell.spielelemente.SpielelementDaten
 import de.seleri.core.domain.modell.spielelemente.spiel.SpielMetaDaten
 
-fun generiereSpielelementDaten(ogSprache: Sprache, lokalisierungEingabe: LokalisierungEingabe): SpielelementDaten =
-	SpielelementDaten(lokalisierung = generiereLokalisierung(ogSprache, lokalisierungEingabe))
+fun SpielelementDaten.Companion.fromSkript(lokalisierung: Lokalisierung): SpielelementDaten =
+	SpielelementDaten(lokalisierung = lokalisierung)
 
-fun generiereSpielMetaDaten(ogSprache: Sprache, spielEingabe: SpielEingabe): SpielMetaDaten =
-	SpielMetaDaten(spielelementDaten = generiereSpielelementDaten(ogSprache, spielEingabe.lokalisierungEingabe))
+fun SpielMetaDaten.Companion.fromSkript(lokalisierung: Lokalisierung): SpielMetaDaten =
+	SpielMetaDaten(SpielelementDaten(lokalisierung = lokalisierung))
