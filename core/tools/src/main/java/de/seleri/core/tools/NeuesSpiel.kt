10f47ae836c394a39c0359d870b3f68b31e05109
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-29 23:54:54 +0200

    Namen der Tabellen angepasst

--- core/tools/src/main/java/de/seleri/core/tools/NeuesSpiel.kt
+++ core/tools/src/main/java/de/seleri/core/tools/NeuesSpiel.kt
@@ -599,8 +599,8 @@ fun main() {
 	dbsToJson(dbs.kartentexte, "kartentexte", outputName)
 	dbsToJson(dbs.lokalisierungen, "lokalisierungen", outputName)
 	dbsToJson(dbs.translationen, "translationen", outputName)
-	dbsToJson(dbs.spielXkategorien, "spielxkategorien", outputName)
-	dbsToJson(dbs.kategorieXkartentexte, "kategoriexkartentexte", outputName)
+	dbsToJson(dbs.spielXkategorien, "spiel_x_kategorie", outputName)
+	dbsToJson(dbs.kategorieXkartentexte, "kategorie_x_kartentext", outputName)
 }
 
 private inline fun <reified T> dbsToJson(tabelle: List<T>, name: String, outputName: String) {
