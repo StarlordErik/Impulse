d0bbe2cf5dec36cd0b1cc60dbbd586ac426f0143
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-30 00:59:37 +0200

    Sprachen haben jetzt IDs!

--- core/common/src/main/java/de/seleri/core/common/Sprache.kt
+++ core/common/src/main/java/de/seleri/core/common/Sprache.kt
@@ -4,5 +4,12 @@ package de.seleri.core.common
 enum class Sprache(val id: Int) { OG(OG_ID),
 	ERIK(ERIK_ID),
 	DE(DE_ID),
-	EN(EN_ID)
+	EN(EN_ID);
+
+
+	companion object {
+
+		fun fromId(id: Int): Sprache =
+			entries.first { it.id == id }
+	}
 }
