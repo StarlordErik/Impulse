7d09899fd664e70ff25ab7b1e617b848af1d329f
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-30 00:00:55 +0200

    Utils-Klasse zum Einlesen der JSONs zur Verfügung gestellt

---
+++ core/di/src/main/java/de/seleri/core/di/utils/JsonDataLoader.kt
@@ -0,0 +1,18 @@
+package de.seleri.core.di.utils
+
+import android.content.Context
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.serializer
+
+class JsonDataLoader {
+
+	inline fun <reified T> loadList(context: Context, fileName: String): List<T> {
+		val input = context.assets
+			.open(fileName)
+			.bufferedReader()
+			.use { it.readText() }
+		val json = Json { ignoreUnknownKeys = true }
+		return json.decodeFromString(ListSerializer(serializer()), input)
+	}
+}
