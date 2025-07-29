84c090330053a5db5d8a15d698f36aa98a10766e
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-29 16:59:12 +0200

    ANZAHL_AN_SPRACHEN aktualisiert sich nun automatisch

--- core/tools/src/main/java/de/seleri/core/tools/fromSkriptUtils/TranslationFromSkript.kt
+++ core/tools/src/main/java/de/seleri/core/tools/fromSkriptUtils/TranslationFromSkript.kt
@@ -4,7 +4,6 @@ import de.seleri.core.common.Sprache
 import de.seleri.core.common.idInt.TranslationIDint
 import de.seleri.core.domain.modell.Translation
 
-const val ANZAHL_AN_SPRACHEN = 4
 fun Translation.Companion.fromSkript(
 	freieTranslationID: Int, sprache: Sprache, bezeichnung: String
 ): Pair<Translation?, Int> {
@@ -26,7 +25,7 @@ fun Translation.Companion.fromSkriptForKartentexte(
 		val translation = translationInfo.first
 		if (translation != null) translationen += translation
 
-		anzahlNeuerTranslationen += (1 + translationInfo.second) * ANZAHL_AN_SPRACHEN
+		anzahlNeuerTranslationen += (1 + translationInfo.second) * Sprache.entries.size
 	}
 
 	val maxID = anzahlNeuerTranslationen - 1
