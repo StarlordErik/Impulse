623efb1db3d4975a7b5525a1358f1db09de17d8b
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-30 01:56:44 +0200

    KategorieRoom überarbeitet

--- core/data/src/main/java/de/seleri/core/data/entities/singles/spielelemente/KategorieRoom.kt
+++ core/data/src/main/java/de/seleri/core/data/entities/singles/spielelemente/KategorieRoom.kt
@@ -2,12 +2,26 @@ package de.seleri.core.data.entities.singles.spielelemente
 
 import androidx.room.Embedded
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import de.seleri.core.data.entities.singles.LokalisierungRoom
+import kotlinx.serialization.Serializable
 
-@Entity(tableName = "Kategorien")
+@Serializable
+@Entity(
+	tableName = "kategorien", foreignKeys = [ForeignKey(
+		entity = LokalisierungRoom::class,
+		parentColumns = ["id"],
+		childColumns = ["lokalisierungID"],
+		onDelete = ForeignKey.CASCADE
+	)], indices = [Index(value = ["lokalisierungID"], unique = true)]
+)
 data class KategorieRoom(
 	@PrimaryKey(autoGenerate = true)
 	override val id: Int,
+
+	override val lokalisierungID: Int,
 
 	@Embedded
 	override val spielelementDatenRoom: SpielelementDatenRoom,
