fda3a0280181748abe60e716fa1ec15d63ca1848
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-30 22:40:01 +0200

    Typ-isierung ftw!

--- core/data/src/main/java/de/seleri/core/data/daos/singles/lokStern/spielelemente/SpielelementDao.kt
+++ core/data/src/main/java/de/seleri/core/data/daos/singles/lokStern/spielelemente/SpielelementDao.kt
@@ -4,7 +4,7 @@ import de.seleri.core.common.id.spielelementID.SpielelementID
 import de.seleri.core.data.daos.singles.lokStern.LokSternDao
 import de.seleri.core.data.entities.singles.lokStern.spielelemente.SpielelementRoom
 
-interface SpielelementDao<S: SpielelementRoom>: LokSternDao<S> {
+interface SpielelementDao<S: SpielelementRoom, SID: SpielelementID>: LokSternDao<S> {
 
-	suspend fun get(spielelementID: SpielelementID): S
+	suspend fun get(spielelementID: SID): S
 }
