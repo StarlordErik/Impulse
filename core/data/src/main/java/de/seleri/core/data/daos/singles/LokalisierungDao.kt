5080f281ba9b19e4b8d96de3f3f4f1785b75586f
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-30 22:07:49 +0200

    Lokalisierung nun für alle get-bar

--- core/data/src/main/java/de/seleri/core/data/daos/singles/LokalisierungDao.kt
+++ core/data/src/main/java/de/seleri/core/data/daos/singles/LokalisierungDao.kt
@@ -3,6 +3,8 @@ package de.seleri.core.data.daos.singles
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import de.seleri.core.common.id.EntityID
 import de.seleri.core.data.daos.EntityDao
 import de.seleri.core.data.entities.singles.LokalisierungRoom
 
@@ -17,4 +19,7 @@ interface LokalisierungDao: EntityDao<LokalisierungRoom> {
 
 	@Delete
 	override suspend fun delete(entity: LokalisierungRoom): Int
+
+	@Query("SELECT * FROM lokalisierungen WHERE id = :id")
+	suspend fun get(id: EntityID): LokalisierungRoom
 }
