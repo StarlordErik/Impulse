e95f1e61097097c286378f8014c519179d6ea436
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-30 22:40:14 +0200

    KartentextDao schreib sich beinah wie von selbst!

--- core/data/src/main/java/de/seleri/core/data/daos/singles/lokStern/spielelemente/KartentextDao.kt
+++ core/data/src/main/java/de/seleri/core/data/daos/singles/lokStern/spielelemente/KartentextDao.kt
@@ -2,23 +2,27 @@ package de.seleri.core.data.daos.singles.lokStern.spielelemente
 
 import androidx.room.Dao
 import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
-import androidx.room.Upsert
+import de.seleri.core.common.id.spielelementID.KartentextID
 import de.seleri.core.data.entities.singles.lokStern.spielelemente.KartentextRoom
 
 @Dao
-interface KartentextDao: SpielelementDao<KartentextRoom> {
+interface KartentextDao: SpielelementDao<KartentextRoom, KartentextID> {
 
-	@Upsert
-	override suspend fun upsert(entity: KartentextRoom): Long
+	@Insert
+	override suspend fun insert(entity: KartentextRoom): Long
 
-	@Update
-	suspend fun update(kartentexte: Collection<KartentextRoom>)
+	@Insert
+	override suspend fun insertAll(entities: Collection<KartentextRoom>): List<Long>
 
 	@Delete
-	override suspend fun delete(entity: KartentextRoom)
+	override suspend fun delete(entity: KartentextRoom): Int
 
-	@Query("SELECT * FROM Kartentexte WHERE id = :spielelementID")
-	override suspend fun get(spielelementID: Int): KartentextRoom
+	@Update
+	override suspend fun update(entity: KartentextRoom): Int
+
+	@Query("SELECT * FROM kartentexte WHERE id = :spielelementID")
+	override suspend fun get(spielelementID: KartentextID): KartentextRoom
 }
