6f4b82beb95761bf09602a2487ddf5bc6821ca01
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-31 03:09:00 +0200

    common entities erstellt

--- core/common/src/main/java/de/seleri/core/common/entities/joins/JoinEntity.kt
+++ core/common/src/main/java/de/seleri/core/common/entities/joins/JoinEntity.kt
@@ -1,9 +1,10 @@
 package de.seleri.core.common.entities.joins
 
+import de.seleri.core.common.entities.Entity
 import de.seleri.core.common.id.spielelementID.BestandteilID
 import de.seleri.core.common.id.spielelementID.SammlungID
 
-interface JoinEntity<SID: SammlungID, BID: BestandteilID> {
+interface JoinEntity<SID: SammlungID, BID: BestandteilID>: Entity {
 
 	val sammlungID: SID
 	val bestandteilID: BID
