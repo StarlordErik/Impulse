86742b93622c089a4793fafae08e97c02b1b2b2b
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-31 17:41:17 +0200

    SammlungsMapper vereinheitlicht

---
+++ core/domain/src/main/java/de/seleri/core/domain/mapper/JoinMapper.kt
@@ -0,0 +1,21 @@
+package de.seleri.core.domain.mapper
+
+import de.seleri.core.common.entities.joins.JoinEntity
+import de.seleri.core.common.ids.spielelementID.BestandteilID
+import de.seleri.core.common.ids.spielelementID.SammlungID
+import de.seleri.core.domain.modell.spielelemente.sammlungen.Bestandteil
+import de.seleri.core.domain.modell.spielelemente.sammlungen.Sammlung
+
+typealias JoinFactory<X, SID, BID> = (
+	sammlungID: SID, bestandteilID: BID
+) -> X
+
+fun <E: JoinEntity<SID, BID>, SID: SammlungID, BID: BestandteilID, B: Bestandteil<BID>> Sammlung<B, SID>.toEntity(
+	factory: JoinFactory<E, SID, BID>
+): Collection<E> =
+	this.bestandteile.map { bestandteil ->
+		factory(
+			this.id, bestandteil.id
+		)
+	}
+
