fa9fe25182f543de63ce04d4a77c334eec83674f
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-31 14:31:36 +0200

    LokalisierungMapper erstellt!

--- core/domain/src/main/java/de/seleri/core/domain/mapper/LokalisierungEtM.kt
+++ core/domain/src/main/java/de/seleri/core/domain/mapper/LokalisierungEtM.kt
@@ -1,16 +1,12 @@
 package de.seleri.core.domain.mapper
 
 import de.seleri.core.common.Sprache
-import de.seleri.core.data.entities.singles.LokalisierungRoom
+import de.seleri.core.common.entities.singles.LokalisierungEntity
 import de.seleri.core.domain.modell.Lokalisierung
 import de.seleri.core.domain.modell.Translation
 
-fun LokalisierungRoom.toDomain(translationen: Map<Sprache, Translation>): Lokalisierung =
+fun LokalisierungEntity.toDomain(translationen: Map<Sprache, Translation>): Lokalisierung =
 	Lokalisierung(
 		id = this.id, ogSprache = this.ogSprache, translationen = translationen
 	)
 
-fun Lokalisierung.toEntity(): LokalisierungRoom =
-	LokalisierungRoom(
-		lokalisierungID = this.id.value, ogSprache = this.ogSprache
-	)
--- core/tools/src/main/java/de/seleri/core/tools/mapper/LokalisierungMtDTO.kt
+++ core/tools/src/main/java/de/seleri/core/tools/mapper/LokalisierungMtDTO.kt
@@ -1,2 +1,9 @@
 package de.seleri.core.tools.mapper
 
+import de.seleri.core.domain.modell.Lokalisierung
+import de.seleri.core.tools.dtos.singles.LokalisierungDTO
+
+fun Lokalisierung.toEntity(): LokalisierungDTO =
+	LokalisierungDTO(
+		lokalisierungID = this.id.value, ogSprache = this.ogSprache
+	)
