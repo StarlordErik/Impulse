215d0115f8cf64cc44fcb888bf32801d436a26dd
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-31 03:25:59 +0200

    Converter für toEntityID nachgeholt

--- core/data/src/main/java/de/seleri/core/data/Converters.kt
+++ core/data/src/main/java/de/seleri/core/data/Converters.kt
@@ -3,12 +3,12 @@ package de.seleri.core.data
 import androidx.room.TypeConverter
 import de.seleri.core.common.Sprache
 import de.seleri.core.common.id.EntityID
+import de.seleri.core.common.id.LokalisierungID
+import de.seleri.core.common.id.spielelementID.KartentextID
+import de.seleri.core.common.id.spielelementID.KategorieID
+import de.seleri.core.common.id.spielelementID.SpielID
 
 class Converters {
-
-	@TypeConverter
-	fun fromEntityID(entityID: EntityID): Int =
-		entityID.id
 
 	@TypeConverter
 	fun fromSprache(sprache: Sprache): Int =
@@ -17,4 +17,24 @@ class Converters {
 	@TypeConverter
 	fun toSprache(spracheID: Int): Sprache =
 		Sprache.fromId(spracheID)
+
+	@TypeConverter
+	fun fromEntityID(entityID: EntityID): Int =
+		entityID.id
+
+	@TypeConverter
+	fun toLokalisierungID(lokalisierungID: Int): LokalisierungID =
+		LokalisierungID(lokalisierungID)
+
+	@TypeConverter
+	fun toKartentextID(kartentextID: Int): KartentextID =
+		KartentextID(kartentextID)
+
+	@TypeConverter
+	fun toKategorieID(kategorieID: Int): KategorieID =
+		KategorieID(kategorieID)
+
+	@TypeConverter
+	fun toSpielID(spielID: Int): SpielID =
+		SpielID(spielID)
 }
