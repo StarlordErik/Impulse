419b33bafa0a0293bb2a67f7a409a9256532300d
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-07-31 16:18:10 +0200

    SpielMetaMapper erstellt

--- core/domain/src/main/java/de/seleri/core/domain/mapper/SpielMetaMapper.kt
+++ core/domain/src/main/java/de/seleri/core/domain/mapper/SpielMetaMapper.kt
@@ -1,2 +1,13 @@
 package de.seleri.core.domain.mapper
 
+import de.seleri.core.common.entities.singles.spielelemente.SpielEntity
+import de.seleri.core.domain.modell.Lokalisierung
+import de.seleri.core.domain.modell.spielelemente.spiel.SpielMetaDaten
+
+object SpielMetaMapper {
+
+	operator fun invoke(dies: SpielEntity, lokalisierung: Lokalisierung): SpielMetaDaten =
+		SpielMetaDaten(
+			spielelementDaten = SpielelementMapper(dies, lokalisierung), bildDateiname = dies.bildDateiname
+		)
+}
