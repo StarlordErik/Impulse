2830aaaa04111f4b466ef7fa5d1f784a55031158
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-08-01 23:11:29 +0200

    TranslationImpl fertig gestellt!

--- core/data/src/main/java/de/seleri/core/data/implementations/TranslationImpl.kt
+++ core/data/src/main/java/de/seleri/core/data/implementations/TranslationImpl.kt
@@ -4,18 +4,11 @@ import de.seleri.core.common.Sprache
 import de.seleri.core.common.ids.LokalisierungID
 import de.seleri.core.data.daos.compositePk.TranslationDAO
 import de.seleri.core.data.toRoom
+import de.seleri.core.domain.mapper.toDomain
 import de.seleri.core.domain.model.Translation
 import de.seleri.core.domain.repositories.TranslationRepo
 
 class TranslationImpl(private val dao: TranslationDAO): TranslationRepo {
-
-	override suspend fun delete(model: Translation): Int {
-		TODO("Not yet implemented")
-	}
-
-	override suspend fun update(model: Translation): Int {
-		TODO("Not yet implemented")
-	}
 
 	override suspend fun new(
 		translation: Translation, lokalisierungID: LokalisierungID, sprache: Sprache
@@ -25,7 +18,25 @@ class TranslationImpl(private val dao: TranslationDAO): TranslationRepo {
 		dao.insert(entity)
 	}
 
+	override suspend fun delete(
+		translation: Translation, lokalisierungID: LokalisierungID, sprache: Sprache
+	): Int {
+		val entity = translation.toRoom(lokalisierungID, sprache)
+
+		return dao.delete(entity)
+	}
+
+	override suspend fun update(
+		translation: Translation, lokalisierungID: LokalisierungID, sprache: Sprache
+	): Int {
+		val entity = translation.toRoom(lokalisierungID, sprache)
+
+		return dao.update(entity)
+	}
+
 	override suspend fun getForLokalisierung(id: LokalisierungID): Collection<Translation> {
-		TODO("Not yet implemented")
+		val entities = dao.getForLokalisierung(id)
+
+		return entities.map { it.toDomain() }
 	}
 }
