87bd932ee39823649222d54805652ec2508d748a
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-08-07 22:47:40 +0200

    ScreenRoute geupdatet

--- core/representation/src/main/java/de/seleri/core/representation/ui/screens/ScreenRoute.kt
+++ core/representation/src/main/java/de/seleri/core/representation/ui/screens/ScreenRoute.kt
@@ -1,11 +1,11 @@
 package de.seleri.core.representation.ui.screens
 
-import de.seleri.core.common.idTypes.SpielelementID
+import de.seleri.core.common.ids.spielelementID.SpielID
 
 sealed class ScreenRoute(val route: String) { object StartscreenRoute: ScreenRoute("start")
 	object SpielscreenRoute: ScreenRoute("spiel") {
 
-		fun mitDerSpielID(spielID: SpielelementID.SpielID) =
-			"$route/${spielID.toInt()}"
+		fun mitDerSpielID(spielID: SpielID) =
+			"$route/${spielID.value}"
 	}
 }
