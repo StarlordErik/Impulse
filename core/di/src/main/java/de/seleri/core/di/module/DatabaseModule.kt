50db0371c2e9e73bef8391a028ee2425303c71aa
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-08-09 16:50:40 +0200

    Callback vernünftig ausgeführt

--- core/di/src/main/java/de/seleri/core/di/module/DatabaseModule.kt
+++ core/di/src/main/java/de/seleri/core/di/module/DatabaseModule.kt
@@ -2,7 +2,6 @@ package de.seleri.core.di.module
 
 import android.content.Context
 import androidx.room.Room
-import androidx.room.RoomDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -28,7 +27,8 @@ object DatabaseModule {
 	@Singleton
 	fun provideDatabase(
 		@ApplicationContext
-		context: Context, callback: RoomDatabase.Callback
+		context: Context,
+		// callback: RoomDatabase.Callback
 	): AppDatabase {
 		return Room
 			.databaseBuilder(
