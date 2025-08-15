b02c6ceec7e2b289199b1c1224e61404d079b216
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2025-08-15 21:26:25 +0200

    Erzählt euch mehr - assets erstellt

--- core/domain/src/main/java/de/seleri/core/domain/useCases/create/CreateDatabaseUC.kt
+++
@@ -1,9 +0,0 @@
-package de.seleri.core.domain.useCases.create
-
-class CreateDatabaseUC(
-	private val createTranslationUC: CreateTranslationUC, private val createLokalisierungUC: CreateLokalisierungUC
-) {
-
-	suspend operator fun invoke() {
-	}
-}
